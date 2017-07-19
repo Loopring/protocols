176fb2d2463b2772d79eb32440152bdb160b8361
wangdong <wangdong@zhongan.io>
2017-07-19 14:47:19 +0800

    add a constant method to get price

--- contracts/LoopringToken.sol
+++ contracts/LoopringToken.sol
@@ -230,7 +230,7 @@ contract LoopringToken is StandardToken {
     /// @dev Compute the amount of LRC token that can be purchased.
     /// @param ethAmount Amount of Ether to purchase LRC.
     /// @return Amount of LRC token to purchase
-    function computeTokenAmount(uint ethAmount) internal returns (uint tokens) {
+    function computeTokenAmount(uint ethAmount) internal constant returns (uint tokens) {
         uint phase = (block.number - firstblock).div(BLOCKS_PER_PHASE);
 
         // A safe check
