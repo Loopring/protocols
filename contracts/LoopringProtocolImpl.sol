56ae2b5dee63a6070e6ae42ad26691eb54b89af0
wangdong <wangdong@zhongan.io>
2017-09-22 06:47:31 -0700

    use Coefficient of Variation to check miner supplied exchange rates

--- contracts/LoopringProtocolImpl.sol
+++ contracts/LoopringProtocolImpl.sol
@@ -47,9 +47,13 @@ contract LoopringProtocolImpl is LoopringProtocol {
 
     uint    public  maxRingSize                 = 0;
     uint    public  ringIndex                   = 0;
-    uint    public  maxPriceRateDeviation       = 0;
 
-    uint    public constant SCALE_AMOUNT        = 10000;
+    // To require all orders' saving ratio fell into a 2.5% Coefficient of
+    // Variation, this number should be set to:
+    //     `sqrt(0.025 * SAVING_RATIO_SCALE)` or 62500. 
+    uint    public  savingRatioCVSThreshold     = 0;
+
+    uint    public constant SAVING_RATIO_SCALE  = 10000;
 
     /// The following two maps are used to keep order fill and cancellation
     /// historical records.
@@ -135,7 +139,7 @@ contract LoopringProtocolImpl is LoopringProtocol {
         address _ringhashRegistryAddress,
         address _delegateAddress,
         uint    _maxRingSize,
-        uint    _maxPriceRateDeviation
+        uint    _savingRatioCVSThreshold
         )
         public {
 
@@ -143,15 +147,15 @@ contract LoopringProtocolImpl is LoopringProtocol {
         require(address(0) != _tokenRegistryAddress);
         require(address(0) != _delegateAddress);
 
-        require(_maxRingSize >= 2);
-        require(_maxPriceRateDeviation >= 1);
+        require(_maxRingSize > 1);
+        require(_savingRatioCVSThreshold > 0);
 
         lrcTokenAddress             = _lrcTokenAddress;
         tokenRegistryAddress        = _tokenRegistryAddress;
         ringhashRegistryAddress     = _ringhashRegistryAddress;
         delegateAddress             = _delegateAddress;
         maxRingSize                 = _maxRingSize;
-        maxPriceRateDeviation       = _maxPriceRateDeviation;
+        savingRatioCVSThreshold       = _savingRatioCVSThreshold;
     }
 
     ////////////////////////////////////////////////////////////////////////////
@@ -456,7 +460,6 @@ contract LoopringProtocolImpl is LoopringProtocol {
         var orders = ring.orders;
         uint ringSize = orders.length;
         uint[] memory priceSavingRateList = new uint[](ringSize);
-        uint savingRateSum = 0;
 
         for (uint i = 0; i < ringSize; i++) {
             uint rateAmountB = orders[i.next(ringSize)].rateAmountS;
@@ -465,16 +468,12 @@ contract LoopringProtocolImpl is LoopringProtocol {
 
             (s0b1 >= b0s1).orThrow("miner supplied exchange rate is invalid");
 
-            priceSavingRateList[i] = s0b1.sub(b0s1).mul(SCALE_AMOUNT).div(s0b1);
-            savingRateSum += priceSavingRateList[i];
+            priceSavingRateList[i] = s0b1.sub(b0s1).mul(SAVING_RATIO_SCALE).div(s0b1);
         }
 
-        uint savingRateAvg = savingRateSum.div(ringSize);
-        uint variance = UintLib.caculateVariance(
-            priceSavingRateList,
-            savingRateAvg);
+        uint cvs = UintLib.cvsquare(priceSavingRateList, SAVING_RATIO_SCALE);
 
-        (variance <= maxPriceRateDeviation)
+        (cvs <= savingRatioCVSThreshold)
             .orThrow("miner supplied exchange rate is invalid");
     }
 
--- contracts/lib/UintLib.sol
+++ contracts/lib/UintLib.sol
@@ -39,27 +39,48 @@ library UintLib {
         return (i + size - 1) % size;
     }
 
-    function caculateVariance(
-        uint[] arr,
-        uint avg
-        )
-        internal
-        constant
-        returns (uint) {
-            
+    /// @dev calculate the square of Coefficient of Variation (CV)
+    /// https://en.wikipedia.org/wiki/Coefficient_of_variation
+    function cvsquare(
+      uint[] arr,
+      uint scale
+      )
+      internal
+      constant
+      returns (uint) {
+
         uint len = arr.length;
-        uint variance = 0;
+        require(len > 1);
+        require(scale > 0);
+
+        uint avg = 0;
         for (uint i = 0; i < len; i++) {
-            uint _sub = 0;
-            if (arr[i] > avg) {
-                _sub = arr[i] - avg;
-            } else {
-                _sub = avg - arr[i];
-            }
-            variance += _sub.mul(_sub);
+          avg += arr[i];
         }
-        variance = variance.div(len);
-        variance = variance.div(avg).div(avg);
-        return variance;
+
+        avg = avg.div(len);
+
+        if (avg == 0) {
+          return 0;
+        }
+
+        uint cvs = 0;
+        for (i = 0; i < len; i++) {
+            uint sub = 0;
+            if (arr[i] > avg) {
+                sub = arr[i] - avg;
+            } else {
+                sub = avg - arr[i];
+            }
+            cvs += sub.mul(sub);
+        }
+        cvs = cvs
+          .mul(scale)
+          .div(avg)
+          .mul(scale)
+          .div(avg)
+          .div(len - 1);
+
+        return cvs;
     }
 }
