d7f9bec6ab39b2f4b73806ac670083d4850a7a0c
wangdong <wangdong@zhongan.io>
2017-09-26 22:19:42 -0700

    bump solidity ver to ^0.4.15

--- contracts/LoopringProtocol.sol
+++ contracts/LoopringProtocol.sol
@@ -15,7 +15,7 @@
   limitations under the License.
 
 */
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.15;
 
 /// @title Loopring Token Exchange Protocol Contract Interface
 /// @author Daniel Wang - <daniel@loopring.org>
