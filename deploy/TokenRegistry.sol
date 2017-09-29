5378fe7e97d277e1f71a525bf6c910aa854f9c17
Kongliang Zhong <kongliang@loopring.org>
2017-09-29 10:43:47 +0800

    add deploy directory

---
+++ deploy/TokenRegistry.sol
@@ -0,0 +1,117 @@
+/*
+
+  Copyright 2017 Loopring Project Ltd (Loopring Foundation).
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+pragma solidity ^0.4.11;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner {
+    if (newOwner != address(0)) {
+      owner = newOwner;
+    }
+  }
+
+}
+
+/// @title Token Register Contract
+/// @author Kongliang Zhong - <kongliang@loopring.org>,
+/// @author Daniel Wang - <daniel@loopring.org>.
+contract TokenRegistry is Ownable {
+
+    address[] public tokens;
+
+    mapping (string => address) tokenSymbolMap;
+
+    function registerToken(address _token, string _symbol)
+        public
+        onlyOwner {
+        require(_token != address(0));
+        require(!isTokenRegisteredBySymbol(_symbol));
+        require(!isTokenRegistered(_token));
+        tokens.push(_token);
+        tokenSymbolMap[_symbol] = _token;
+    }
+
+    function unregisterToken(address _token, string _symbol)
+        public
+        onlyOwner {
+        require(tokenSymbolMap[_symbol] == _token);
+        delete tokenSymbolMap[_symbol];
+        for (uint i = 0; i < tokens.length; i++) {
+            if (tokens[i] == _token) {
+                tokens[i] == tokens[tokens.length - 1];
+                tokens.length --;
+                break;
+            }
+        }
+    }
+
+    function isTokenRegisteredBySymbol(string symbol)
+        public
+        constant
+        returns (bool) {
+        return tokenSymbolMap[symbol] != address(0);
+    }
+
+    function isTokenRegistered(address _token)
+        public
+        constant
+        returns (bool) {
+
+        for (uint i = 0; i < tokens.length; i++) {
+            if (tokens[i] == _token) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getAddressBySymbol(string symbol)
+        public
+        constant
+        returns (address) {
+        return tokenSymbolMap[symbol];
+    }
+}
