bd0ef3dbc730f7e7bc7f2fc7afd90d9b842cc8be
kongliangzhong <kongliangzhong@gmail.com>
2017-11-06 16:55:45 +0800

    fix a bug in TokenRegistry

--- contracts/TokenRegistry.sol
+++ contracts/TokenRegistry.sol
@@ -52,7 +52,7 @@ contract TokenRegistry is Ownable {
         delete tokenMap[_token];
         for (uint i = 0; i < tokens.length; i++) {
             if (tokens[i] == _token) {
-                tokens[i] == tokens[tokens.length - 1];
+                tokens[i] = tokens[tokens.length - 1];
                 tokens.length --;
                 break;
             }
