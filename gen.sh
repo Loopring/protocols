c6e7e1bca223d1f927cc51695541d5a1370a59a2
Benjamin Price <github@benprice.ca>
2017-11-10 03:27:13 -0500

    Add uglifyify

--- gen.sh
+++ gen.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-browserify  -r ./src/ens.js:ens  > ./dist/ens.js
-browserify  -r ./src/hex-utils.js:hex-utils  > ./dist/hex-utils.js
-browserify  -r ./src/keystore.js:keystore  > ./dist/keystore.js
-browserify  -r ./src/privateKey.js:privateKey  > ./dist/privateKey.js
-browserify  -r ./src/validator.js:validator  > ./dist/validator.js
-browserify  -r ./src/order.js:order  > ./dist/order.js
-browserify -r ethereumjs-util -r ./src/relay.js:relay  > ./dist/relay.js
+browserify -g uglifyify -t uglifyify -r ./src/ens.js:ens > ./dist/ens.js
+browserify -g uglifyify -t uglifyify -r ./src/hex-utils.js:hex-utils > ./dist/hex-utils.js
+browserify -g uglifyify -t uglifyify -r ./src/keystore.js:keystore > ./dist/keystore.js
+browserify -g uglifyify -t uglifyify -r ./src/privateKey.js:privateKey > ./dist/privateKey.js
+browserify -g uglifyify -t uglifyify -r ./src/validator.js:validator > ./dist/validator.js
+browserify -g uglifyify -t uglifyify -r ./src/order.js:order > ./dist/order.js
+browserify -g uglifyify -t uglifyify -r ./src/signer.js:signer > ./dist/signer.js
+browserify -g uglifyify -t uglifyify -r ./src/decrypt.js:decrypt > ./dist/decrypt.js
+browserify -g uglifyify -t uglifyify -r ethereumjs-util -r ./src/relay.js:relay > ./dist/relay.js
