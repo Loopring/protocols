fea7e953d26f5114269e88417e549fb883bd676b
Brechtpd <Brechtp.Devos@gmail.com>
2017-11-13 05:43:58 +0100

    Only check available LRC balances when needed (#126)

--- contracts/LoopringProtocolImpl.sol
+++ contracts/LoopringProtocolImpl.sol
@@ -593,27 +593,26 @@ contract LoopringProtocolImpl is LoopringProtocol {
         private
         view
     {
-        uint minerLrcSpendable = getSpendable(
-            delegate,
-            _lrcTokenAddress,
-            feeRecipient
-        );
+        bool checkedMinerLrcSpendable = false;
+        uint minerLrcSpendable = 0;
         uint8 _marginSplitPercentageBase = MARGIN_SPLIT_PERCENTAGE_BASE;
 
         for (uint i = 0; i < ringSize; i++) {
             var state = orders[i];
             var next = orders[(i + 1) % ringSize];
 
-            uint lrcSpendable = getSpendable(
-                delegate,
-                _lrcTokenAddress,
-                state.order.owner
-            );
+            if (state.lrcFee > 0) {
+                uint lrcSpendable = getSpendable(
+                    delegate,
+                    _lrcTokenAddress,
+                    state.order.owner
+                );
 
-            // If order doesn't have enough LRC, set margin split to 100%.
-            if (lrcSpendable < state.lrcFee) {
-                state.lrcFee = lrcSpendable;
-                state.order.marginSplitPercentage = _marginSplitPercentageBase;
+                // If order doesn't have enough LRC, set margin split to 100%.
+                if (lrcSpendable < state.lrcFee) {
+                    state.lrcFee = lrcSpendable;
+                    state.order.marginSplitPercentage = _marginSplitPercentageBase;
+                }
             }
 
             // When an order's LRC fee is 0 or smaller than the specified fee,
@@ -623,6 +622,13 @@ contract LoopringProtocolImpl is LoopringProtocol {
             }
 
             if (state.feeSelection == FEE_SELECT_MARGIN_SPLIT || state.lrcFee == 0) {
+
+                // Only check the available miner balance when absolutely needed
+                if (!checkedMinerLrcSpendable && minerLrcSpendable < state.lrcFee) {
+                    minerLrcSpendable += getSpendable(delegate, _lrcTokenAddress, feeRecipient);
+                    checkedMinerLrcSpendable = true;
+                }
+
                 // Only calculate split when miner has enough LRC;
                 // otherwise all splits are 0.
                 if (minerLrcSpendable >= state.lrcFee) {
@@ -657,7 +663,8 @@ contract LoopringProtocolImpl is LoopringProtocol {
                     // be paid LRC reward first, so the orders in the ring does
                     // mater.
                     if (split > 0) {
-                        minerLrcSpendable = minerLrcSpendable.sub(state.lrcFee);
+                        // minerLrcSpendable >= state.lrcFee
+                        minerLrcSpendable -= state.lrcFee;
                         state.lrcReward = state.lrcFee;
                     }
                     state.lrcFee = 0;
