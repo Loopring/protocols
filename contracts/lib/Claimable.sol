d98a97dcb3dae0ce2a7e4417571c147dbdf2ce73
wangdong <wangdong@zhongan.io>
2017-11-21 10:27:08 -0800

    minor change

--- contracts/lib/Claimable.sol
+++ contracts/lib/Claimable.sol
@@ -35,6 +35,7 @@ contract Claimable is Ownable {
     /// @dev Allows the current owner to set the pendingOwner address.
     /// @param newOwner The address to transfer ownership to.
     function transferOwnership(address newOwner) onlyOwner public {
+        require(newOwner != 0x0 && newOwner != owner);
         pendingOwner = newOwner;
     }
 
