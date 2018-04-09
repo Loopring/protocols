6250388b907d0e5d86be19dd26300809897d1784
kongliangzhong <kongliangzhong@gmail.com>
2018-04-09 16:32:10 +0800

    rename TokenMint to TokenCreator

--- migrations/2_deploy_registries.js
+++ migrations/2_deploy_registries.js
@@ -1,14 +1,14 @@
 var TokenRegistry           = artifacts.require("./TokenRegistry");
 var TokenTransferDelegate   = artifacts.require("./TokenTransferDelegate");
 var NameRegistry            = artifacts.require("./NameRegistry");
-var TokenMint               = artifacts.require("./TokenMint");
+var TokenCreator            = artifacts.require("./TokenCreator");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(TokenTransferDelegate);
   deployer.deploy(NameRegistry);
 
-  deployer.deploy(TokenMint).then(function() {
-    return deployer.deploy(TokenRegistry, TokenMint.address);
+  deployer.deploy(TokenCreator).then(function() {
+    return deployer.deploy(TokenRegistry, TokenCreator.address);
   });
 
 };
