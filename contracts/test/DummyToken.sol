3de1b4fdb22a65d8bba5e802dedba63b365ed52b
kongliangzhong <kongliangzhong@gmail.com>
2018-04-10 11:59:08 +0800

    remove dependency: zepplin-solidity

--- contracts/test/DummyToken.sol
+++ contracts/test/DummyToken.sol
@@ -1,12 +1,21 @@
 pragma solidity 0.4.21;
 
-import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "../lib/ERC20.sol";
+import "../lib/MathUint.sol";
 
+contract DummyToken is ERC20 {
+    using MathUint for uint;
 
-contract DummyToken is MintableToken {
     string public name;
     string public symbol;
     uint8  public decimals;
+    uint   public totalSupply_;
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     function DummyToken(
         string _name,
@@ -27,7 +36,6 @@ contract DummyToken is MintableToken {
         address _target,
         uint _value
         )
-        onlyOwner
         public
     {
         uint currBalance = balanceOf(_target);
@@ -38,4 +46,107 @@ contract DummyToken is MintableToken {
         }
         balances[_target] = _value;
     }
+
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    /**
+    * @dev transfer token for a specified address
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(
+        address _to,
+        uint256 _value
+        )
+        public
+        returns (bool)
+    {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param _owner The address to query the the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address _owner)
+        public
+        view
+        returns (uint256 balance)
+    {
+        return balances[_owner];
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+        )
+        public
+        returns (bool)
+    {
+        require(_to != address(0));
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     *
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param _spender The address which will spend the funds.
+     * @param _value The amount of tokens to be spent.
+     */
+    function approve(
+        address _spender,
+        uint256 _value
+        )
+        public
+        returns (bool)
+    {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param _owner address The address which owns the funds.
+     * @param _spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(
+        address _owner,
+        address _spender)
+        public
+        view
+        returns (uint256)
+    {
+        return allowed[_owner][_spender];
+    }
+
 }
