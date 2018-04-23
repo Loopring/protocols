bd110df472ca9a3cf3ef554fffad9e435c67ffb3
kongliangzhong <kongliangzhong@gmail.com>
2018-04-23 15:21:14 +0800

    fix test failure

--- contracts/TokenRegistry.sol
+++ contracts/TokenRegistry.sol
@@ -29,7 +29,7 @@ contract TokenRegistry {
 
     event TokenUnregistered(
         address indexed addr,
-        string  indexed symbol
+        string          symbol
     );
 
     function registerToken(
