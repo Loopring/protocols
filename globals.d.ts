73d574e599c12b2670e64511bd22302a5249b3ab
kongliangzhong <kongliangzhong@gmail.com>
2018-05-03 16:36:53 +0800

    upgrade typescript version

--- globals.d.ts
+++ globals.d.ts
@@ -1,5 +1,6 @@
 declare module 'bn.js';
 declare module 'ethereumjs-abi';
+declare module 'ethereumjs-util';
 declare module 'es6-promisify';
 
 // Truffle injects the following into the global scope
@@ -11,31 +12,3 @@ declare var beforeEach: any;
 declare var describe: any;
 declare var it: any;
 declare var assert: any;
-
-declare module 'ethereumjs-util' {
-  function bufferToHex(value: Buffer): string;
-  function ecrecover(msgHash: Buffer, v: number, r: Buffer, s: Buffer): Buffer;
-  function fromRpcSig(sig: string): {v: number, r: Buffer, s: Buffer};
-  function hashPersonalMessage(hash: Buffer): Buffer;
-  function isHexString(value: any): boolean;
-  function pubToAddress(pubKey: Buffer, sanitize?: boolean): Buffer;
-  function setLength(a: Buffer, length: number): Buffer;
-  function setLengthLeft(a: Buffer, length: number): Buffer;
-  function sha3(a: Buffer|string|number, bits?: number): Buffer;
-  function toBuffer(value: any): Buffer;
-  function isValidAddress(address: string): boolean;
-
-  export = {
-    bufferToHex,
-    ecrecover,
-    fromRpcSig,
-    hashPersonalMessage,
-    isHexString,
-    pubToAddress,
-    setLength,
-    setLengthLeft,
-    sha3,
-    toBuffer,
-    isValidAddress,
-  };
-}
