afc8bacd643f38e99be7e33f41b037435a680f7d
lyd <lyddream@126.com>
2018-05-17 11:25:33 +0800

    update jsonrpc

--- src/relay/rpc/order.js
+++ src/relay/rpc/order.js
@@ -6,82 +6,86 @@ import {soliditySHA3} from 'ethereumjs-abi'
 import validator from '../validator'
 import {toBN} from "../../common/formatter";
 
-/**
- * @description Get loopring order list.
- * @param host
- * @param filter
- * @returns {Promise.<*>}
- */
-export function getOrders(host, filter) {
-  try {
-    validator.validate({value: filter.delegateAddress, type: 'ETH_ADDRESS'});
-    validator.validate({value: filter.pageIndex, type: 'OPTION_NUMBER'});
-    filter.market && validator.validate({value: filter.market, type: 'STRING'});
-    filter.owner && validator.validate({value: filter.owner, type: 'ETH_ADDRESS'});
-    filter.orderHash && validator.validate({value: filter.orderHash, type: 'STRING'});
-    filter.pageSize && validator.validate({value: filter.pageSize, type: 'OPTION_NUMBER'});
-  } catch (e) {
-    return  Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg));
-  }
-  const body = {};
-  body.method = 'loopring_getOrders';
-  body.params = [filter];
-  body.id = id();
-  return request(host, {
-    method: 'post',
-    body,
-  })
-}
 
-/**
- * @description Get cut off time of the address.
- * @param host
- * @param address
- * @param delegateAddress
- * @param blockNumber
- * @returns {Promise.<*>}
- */
-export function getCutoff(host, {address, delegateAddress, blockNumber}) {
-  blockNumber = blockNumber || 'latest';
-  try {
-    validator.validate({value: address, type: 'ETH_ADDRESS'});
-    validator.validate({value: delegateAddress, type: 'ETH_ADDRESS'});
-    validator.validate({value: blockNumber, type: 'RPC_TAG'});
-  } catch (e) {
-    return  Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
-  }
-  const body = {};
-  body.method = 'loopring_getCutoff';
-  body.params = [{address, delegateAddress, blockNumber}];
-  body.id = id();
-  return request(host, {
-    method: 'post',
-    body,
-  })
-}
+export class Order{
 
-/**
- * @description  Submit an order.The order is submitted to relay as a JSON object,
- * this JSON will be broadcast into peer-to-peer network for off-chain order-book maintainance and ring-ming.
- * Once mined, the ring will be serialized into a transaction and submitted to Ethereum blockchain.
- * @param host relay host
- * @param order
- * @returns {Promise.<*>}
- */
-export function placeOrder(host, {order}) {
-  try {
-    validator.validate({value: order, type: "Order"});
-  } catch(e) {
-    return  Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
+  constructor (host){
+    this.host = host;
   }
-  const body = {};
-  body.method = 'loopring_submitOrder';
-  body.params = [order];
-  body.id = id();
-  return request(host, {
-    method: 'post',
-    body,
-  })
+  /**
+   * @description Get loopring order list.
+   * @param filter
+   * @returns {Promise.<*>}
+   */
+ getOrders(filter) {
+    try {
+      validator.validate({value: filter.delegateAddress, type: 'ETH_ADDRESS'});
+      validator.validate({value: filter.pageIndex, type: 'OPTION_NUMBER'});
+      filter.market && validator.validate({value: filter.market, type: 'STRING'});
+      filter.owner && validator.validate({value: filter.owner, type: 'ETH_ADDRESS'});
+      filter.orderHash && validator.validate({value: filter.orderHash, type: 'STRING'});
+      filter.pageSize && validator.validate({value: filter.pageSize, type: 'OPTION_NUMBER'});
+    } catch (e) {
+      return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg));
+    }
+    const body = {};
+    body.method = 'loopring_getOrders';
+    body.params = [filter];
+    body.id = id();
+    return request(this.host, {
+      method: 'post',
+      body,
+    })
+  }
+
+  /**
+   * @description Get cut off time of the address.
+   * @param address
+   * @param delegateAddress
+   * @param blockNumber
+   * @returns {Promise.<*>}
+   */
+ getCutoff({address, delegateAddress, blockNumber}) {
+    blockNumber = blockNumber || 'latest';
+    try {
+      validator.validate({value: address, type: 'ETH_ADDRESS'});
+      validator.validate({value: delegateAddress, type: 'ETH_ADDRESS'});
+      validator.validate({value: blockNumber, type: 'RPC_TAG'});
+    } catch (e) {
+      return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
+    }
+    const body = {};
+    body.method = 'loopring_getCutoff';
+    body.params = [{address, delegateAddress, blockNumber}];
+    body.id = id();
+    return request(this.host, {
+      method: 'post',
+      body,
+    })
+  }
+
+  /**
+   * @description  Submit an order.The order is submitted to relay as a JSON object,
+   * this JSON will be broadcast into peer-to-peer network for off-chain order-book maintainance and ring-ming.
+   * Once mined, the ring will be serialized into a transaction and submitted to Ethereum blockchain.
+   * @param order
+   * @returns {Promise.<*>}
+   */
+  placeOrder(order) {
+    try {
+      validator.validate({value: order, type: "Order"});
+    } catch(e) {
+      return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
+    }
+    const body = {};
+    body.method = 'loopring_submitOrder';
+    body.params = [order];
+    body.id = id();
+    return request(this.host, {
+      method: 'post',
+      body,
+    })
+  }
 }
 
 
--- src/relay/rpc/ring.js
+++ src/relay/rpc/ring.js
@@ -5,96 +5,102 @@ import Response from '../../common/response'
 import code from "../../common/code"
 import {toBig, toHex} from "../../common/formatter";
 
-/**
- * @description Get all mined rings.
- * @param host
- * @param filter
- * @returns {Promise.<*>}
- */
-export function getRings(host, filter) {
-  try {
-    validator.validate({value: filter.delegateAddress, type: 'ETH_ADDRESS'});
-    if (filter.pageIndex) {
-      validator.validate({value: filter.pageIndex, type: 'OPTION_NUMBER'})
-    }
-    if (filter.pageSize) {
-      validator.validate({value: filter.pageSize, type: 'OPTION_NUMBER'})
-    }
-  } catch (e) {
-    return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
-  }
-  const body = {};
-  body.method = 'loopring_getRingMined';
-  body.params = [filter];
-  body.id = id();
-  return request(host, {
-    method: 'post',
-    body,
-  })
-}
 
-/**
- * @description Get ring mined detail
- * @param host
- * @param ringIndex
- * @returns {Promise}
- */
-export function getRingMinedDetail(host, {ringIndex, protocolAddress}) {
-  try {
-    validator.validate({value: protocolAddress, type: 'ETH_ADDRESS'});
-  } catch (e) {
-    return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
-  }
-  ringIndex = toHex(toBig(ringIndex));
-  const body = {};
-  body.method = 'loopring_getRingMinedDetail';
-  body.params = [{ringIndex}];
-  body.id = id();
-  return request(host, {
-    method: 'post',
-    headers,
-    body,
-  })
-}
 
-/**
- * @description Get order fill history. This history consists of OrderFilled events.
- * @param host
- * @param filter {market, owner, delegateAddress, orderHash, ringHash,pageIndex,pageSize}
- * @returns {Promise}
- */
-export function getFills(host, filter) {
-  try {
-    if (filter.delegateAddress) {
+export default class Ring{
+
+  constructor (host){
+    this.host = host;
+  }
+
+  /**
+   * @description Get all mined rings.
+   * @param filter
+   * @returns {Promise.<*>}
+   */
+  getRings(filter) {
+    try {
       validator.validate({value: filter.delegateAddress, type: 'ETH_ADDRESS'});
+      if (filter.pageIndex) {
+        validator.validate({value: filter.pageIndex, type: 'OPTION_NUMBER'})
+      }
+      if (filter.pageSize) {
+        validator.validate({value: filter.pageSize, type: 'OPTION_NUMBER'})
+      }
+    } catch (e) {
+      return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
     }
-    if (filter.owner) {
-      validator.validate({value: filter.owner, type: 'ETH_ADDRESS'});
-    }
-    if (filter.orderHash) {
-      validator.validate({value: filter.orderHash, type: 'HASH'});
-    }
-    if (filter.ringHash) {
-      validator.validate({value: filter.ringHash, type: 'HASH'});
-    }
-    if (filter.pageIndex) {
-      validator.validate({value: filter.pageIndex, type: 'OPTION_NUMBER'})
-    }
-    if (filter.pageSize) {
-      validator.validate({value: filter.pageSize, type: 'OPTION_NUMBER'})
-    }
-  } catch {
-    return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
+    const body = {};
+    body.method = 'loopring_getRingMined';
+    body.params = [filter];
+    body.id = id();
+    return request(this.host, {
+      method: 'post',
+      body,
+    })
   }
-  const body = {};
-  body.method = 'loopring_getFills';
-  body.params = [filter];
-  body.id = id();
-  return request(host, {
-    method: 'post',
-    headers,
-    body,
-  })
+
+  /**
+   * @description Get ring mined detail
+   * @param ringIndex
+   * @returns {Promise}
+   */
+ getRingMinedDetail({ringIndex, protocolAddress}) {
+    try {
+      validator.validate({value: protocolAddress, type: 'ETH_ADDRESS'});
+    } catch (e) {
+      return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
+    }
+    ringIndex = toHex(toBig(ringIndex));
+    const body = {};
+    body.method = 'loopring_getRingMinedDetail';
+    body.params = [{ringIndex}];
+    body.id = id();
+    return request(this.host, {
+      method: 'post',
+      body,
+    })
+  }
+
+  /**
+   * @description Get order fill history. This history consists of OrderFilled events.
+   * @param filter {market, owner, delegateAddress, orderHash, ringHash,pageIndex,pageSize}
+   * @returns {Promise}
+   */
+  getFills(filter) {
+    try {
+      if (filter.delegateAddress) {
+        validator.validate({value: filter.delegateAddress, type: 'ETH_ADDRESS'});
+      }
+      if (filter.owner) {
+        validator.validate({value: filter.owner, type: 'ETH_ADDRESS'});
+      }
+      if (filter.orderHash) {
+        validator.validate({value: filter.orderHash, type: 'HASH'});
+      }
+      if (filter.ringHash) {
+        validator.validate({value: filter.ringHash, type: 'HASH'});
+      }
+      if (filter.pageIndex) {
+        validator.validate({value: filter.pageIndex, type: 'OPTION_NUMBER'})
+      }
+      if (filter.pageSize) {
+        validator.validate({value: filter.pageSize, type: 'OPTION_NUMBER'})
+      }
+    } catch {
+      return Promise.resolve(new Response(code.PARAM_INVALID.code, code.PARAM_INVALID.msg))
+    }
+    const body = {};
+    body.method = 'loopring_getFills';
+    body.params = [filter];
+    body.id = id();
+    return request(this.host, {
+      method: 'post',
+      body,
+    })
+  }
 }
+
+
 
 
