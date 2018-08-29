85612a06e9b3594ec5116e4ca7ad6e0bf5fb7dcf
Brecht <Brechtp.Devos@gmail.com>
2018-08-30 01:58:50 +0200

    Added solidity-coverage

--- truffle.js
+++ truffle.js
@@ -32,6 +32,13 @@ module.exports = {
       gasPrice: 21000000000,
       gas: 6700000
     },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    },
     docker: {
       host: "testrpc",
       port: 8545,
