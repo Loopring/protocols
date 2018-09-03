6c48f57912d49b95919aebf2c66027eab64c36d7
Brecht <Brechtp.Devos@gmail.com>
2018-09-03 15:59:13 +0200

    Fixed formatting for linter

--- contracts/impl/Voting.sol
+++ contracts/impl/Voting.sol
@@ -1,51 +1,75 @@
-pragma solidity ^0.4.24; 
+pragma solidity ^0.4.24;
 
 import "../lib/ERC20.sol";
 
 contract Voting {
-  /* 
-  The key of the mapping is candidate name stored as type bytes32 and value is
-  an unsigned integer which used to store the vote count
-  */
-  mapping (bytes32 => uint256) public votesReceived;
+    /*
+    The key of the mapping is candidate name stored as type bytes32 and value is
+    an unsigned integer which used to store the vote count
+    */
+    mapping (bytes32 => uint256) public votesReceived;
 
-  mapping (address => bool) public voteFlag;
-  
-  address lrcAddress = 0xEF68e7C694F40c8202821eDF525dE3782458639f;
+    mapping (address => bool) public voteFlag;
 
-  bytes32[] public candidateList;
+    address lrcAddress = 0xEF68e7C694F40c8202821eDF525dE3782458639f;
 
-  constructor(bytes32[] candidateNames) public {
-    candidateList = candidateNames;
-  }
+    bytes32[] public candidateList;
 
-  function totalVotesFor(bytes32 candidate) public view returns (uint256) {
-    require(validCandidate(candidate));
-
-    return votesReceived[candidate];
-  }
-
-  function voteForCandidate(bytes32 candidate) public {
-    require(validCandidate(candidate));
-    require(voteFlag[msg.sender] != true);
-
-    uint256 balance = ERC20(lrcAddress).balanceOf(msg.sender);
-    votesReceived[candidate] += balance;
-    voteFlag[msg.sender] == true;
-  }
-
-  function validCandidate(bytes32 candidate) public view returns (bool) {
-    for(uint i = 0; i < candidateList.length; i++) {
-      if (candidateList[i] == candidate) {
-        return true;
-      }
+    constructor(
+        bytes32[] candidateNames
+        )
+        public
+    {
+        candidateList = candidateNames;
     }
-    
-    return false;
-  }
 
-  // This function returns the list of candidates.
-  function getCandidateList() public view returns (bytes32[]) {
-    return candidateList;
-  }
+    function totalVotesFor(
+        bytes32 candidate
+        )
+        public
+        view
+        returns (uint256)
+    {
+        require(validCandidate(candidate));
+
+        return votesReceived[candidate];
+    }
+
+    function voteForCandidate(
+        bytes32 candidate
+        )
+        public
+    {
+        require(validCandidate(candidate));
+        require(voteFlag[msg.sender] != true);
+
+        uint256 balance = ERC20(lrcAddress).balanceOf(msg.sender);
+        votesReceived[candidate] += balance;
+        voteFlag[msg.sender] == true;
+    }
+
+    function validCandidate(
+        bytes32 candidate
+        )
+        public
+        view
+        returns (bool)
+    {
+        for(uint i = 0; i < candidateList.length; i++) {
+            if (candidateList[i] == candidate) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // This function returns the list of candidates.
+    function getCandidateList()
+        public
+        view
+        returns (bytes32[])
+    {
+        return candidateList;
+    }
 }
