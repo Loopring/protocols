6fba432ad7e028d0a851e3bf78d49298b66f08a6
kongliangzhong <kongliangzhong@gmail.com>
2018-09-12 15:03:07 +0800

    delete tokenRegistry and symbolRegistry

--- src/artifacts.ts
+++ src/artifacts.ts
@@ -1,7 +1,5 @@
 
 export class Artifacts {
-  public TokenRegistry: any;
-  public SymbolRegistry: any;
   public RingSubmitter: any;
   public RingCanceller: any;
   public TradeDelegate: any;
@@ -9,15 +7,12 @@ export class Artifacts {
   public OrderRegistry: any;
   public MinerRegistry: any;
   public DummyToken: any;
-  public DummyAgency: any;
   public DummyBrokerInterceptor: any;
   public DummyExchange: any;
   public FeeHolder: any;
   public OrderBook: any;
   public TaxTable: any;
   constructor(artifacts: any) {
-    this.TokenRegistry = artifacts.require("impl/TokenRegistry");
-    this.SymbolRegistry = artifacts.require("impl/SymbolRegistry");
     this.RingSubmitter = artifacts.require("impl/RingSubmitter");
     this.RingCanceller = artifacts.require("impl/RingCanceller");
     this.TradeDelegate = artifacts.require("impl/TradeDelegate");
@@ -25,7 +20,6 @@ export class Artifacts {
     this.OrderRegistry = artifacts.require("impl/OrderRegistry");
     this.MinerRegistry = artifacts.require("impl/MinerRegistry");
     this.DummyToken = artifacts.require("test/DummyToken");
-    this.DummyAgency = artifacts.require("test/DummyAgency");
     this.DummyBrokerInterceptor = artifacts.require("test/DummyBrokerInterceptor");
     this.DummyExchange = artifacts.require("test/DummyExchange");
     this.FeeHolder = artifacts.require("impl/FeeHolder");
