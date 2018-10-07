d80c2683c11f574b3f1335afde0c5a0cf0927846
Brecht <Brechtp.Devos@gmail.com>
2018-10-07 23:25:09 +0200

    Added OrderBook test

--- test/testOrderBook.ts
+++ test/testOrderBook.ts
@@ -1,6 +1,6 @@
 import { BN } from "bn.js";
 import abi = require("ethereumjs-abi");
-import {OrderInfo, OrderUtil } from "protocol2-js";
+import { expectThrow, OrderInfo, OrderUtil } from "protocol2-js";
 import util = require("util");
 import { Artifacts } from "../util/Artifacts";
 
@@ -58,11 +58,14 @@ contract("OrderBook", (accounts: string[]) => {
   };
 
   before(async () => {
-    orderBook = await OrderBook.deployed();
-
     lrcAddress = LRCToken.address;
     rdnAddress = RDNToken.address;
     gtoAddress = GTOToken.address;
+  });
+
+  beforeEach(async () => {
+    // New OrderBook for each test
+    orderBook = await OrderBook.new();
   });
 
   describe("any user", () => {
@@ -116,6 +119,39 @@ contract("OrderBook", (accounts: string[]) => {
 
       const submitted = await orderBook.orderSubmitted(orderHash);
       assert.equal(submitted, true, "order should be submitted");
+    });
+
+    it("should not be able to submit the same order twice", async () => {
+      const validSince = web3.eth.getBlock(web3.eth.blockNumber).timestamp - 1000;
+      const validUntil = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 360000;
+
+      const orderInfo: OrderInfo = {
+        owner: orderOwner,
+        tokenS: lrcAddress,
+        tokenB: gtoAddress,
+        broker: "0xdca66846a7123afe448f13f013ee83fbc33344e3",
+        amountS: 1e+22,
+        amountB: 3000000000000000000,
+        feeAmount: 1000000000000000000,
+        feePercentage: 20,
+        dualAuthSignAlgorithm: 0,
+        allOrNone: true,
+        validSince: 1669907153,
+        validUntil: 1769907153,
+        walletAddr: "0xdca66846a7123afe448f13f013ee83fbc33344e3",
+        walletSplitPercentage: 10,
+        tokenRecipient: "0xa826c89cb23f99d8e2a754d1a85c13b37309b722",
+        feeToken: "0x40efda0416446e83cdc6ec3d143bec4f82827478",
+        waiveFeePercentage: 0,
+        tokenSFeePercentage: 0,
+        tokenBFeePercentage: 0,
+        onChain: true,
+      };
+
+      const orderUtil = new OrderUtil(undefined);
+      const bytes32Array = orderUtil.toOrderBookSubmitParams(orderInfo);
+      await orderBook.submitOrder(bytes32Array, {from: orderOwner});
+      await expectThrow(orderBook.submitOrder(bytes32Array, {from: orderOwner}));
     });
   });
 
