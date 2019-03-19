2d536685568b27a4dea00a4e7059aaba5dd58cba
kongliangzhong <kongliangzhong@gmail.com>
2019-03-19 20:42:48 +0800

    add index.js

--- packages/loopring_v2.js/packages/contract-artifacts/src/index.js
+++ packages/loopring_v2.js/packages/contract-artifacts/src/index.js
@@ -1,28 +1,49 @@
-// UMD
-;
-(function(name, definition) {
-  var theModule = definition(),
-    // this is considered "safe":
-    hasDefine = typeof define === 'function' && define.amd,
-    // hasDefine = typeof define === 'function',
-    hasExports = typeof module !== 'undefined' && module.exports;
+import * as RingSubmitter from '../artifacts/RingSubmitter.json';
+import * as OrderCanceller from '../artifacts/OrderCanceller.json';
+import * as TradeDelegate from '../artifacts/TradeDelegate.json';
+import * as TradeHistory from '../artifacts/TradeHistory.json';
+import * as BrokerRegistry from '../artifacts/BrokerRegistry.json';
+import * as OrderRegistry from '../artifacts/OrderRegistry.json';
+import * as DummyBrokerInterceptor from '../artifacts/DummyBrokerInterceptor.json';
+import * as DummyExchange from '../artifacts/DummyExchange.json';
+import * as DummyBurnManager from '../artifacts/DummyBurnManager.json';
+import * as FeeHolder from '../artifacts/FeeHolder.json';
+import * as OrderBook from '../artifacts/OrderBook.json';
+import * as BurnRateTable from '../artifacts/BurnRateTable.json';
+import * as BurnManager from '../artifacts/BurnManager.json';
+import * as DummyToken from '../artifacts/DummyToken.json';
+import * as LRCToken from '../artifacts/LRC.json';
+import * as GTOToken from '../artifacts/GTO.json';
+import * as RDNToken from '../artifacts/RDN.json';
+import * as REPToken from '../artifacts/REP.json';
+import * as WETHToken from '../artifacts/WETH.json';
+import * as INDAToken from '../artifacts/INDA.json';
+import * as INDBToken from '../artifacts/INDB.json';
+import * as TESTToken from '../artifacts/TEST.json';
+import * as DeserializerTest from '../artifacts/DeserializerTest.json';
 
-  if (hasDefine) { // AMD Module
-    define(theModule);
-  } else if (hasExports) { // Node.js Module
-    module.exports = theModule;
-  } else { // Assign to common namespaces or simply the global object (window)
-    (this.jQuery || this.ender || this.$ || this)[name] = theModule;
-  }
-})('contract-artifacts', function() {
-  var module = this;
-  module.plugins = [];
-  module.highlightColor = "yellow";
-  module.errorColor = "red";
-
-  // Exposed public methods
-  return {
-    highlightColor: module.highlightColor
-  };
-
-});
+export {
+  RingSubmitter,
+  OrderCanceller,
+  TradeDelegate,
+  TradeHistory,
+  BrokerRegistry,
+  OrderRegistry,
+  DummyBrokerInterceptor,
+  DummyExchange,
+  DummyBurnManager,
+  FeeHolder,
+  OrderBook,
+  BurnRateTable,
+  BurnManager,
+  DummyToken,
+  LRCToken,
+  GTOToken,
+  RDNToken,
+  REPToken,
+  WETHToken,
+  INDAToken,
+  INDBToken,
+  TESTToken,
+  DeserializerTest,
+};
