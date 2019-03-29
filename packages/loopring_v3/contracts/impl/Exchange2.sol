0143efa2780880f628fbd92446e4502634ad08dc
Daniel Wang <dong77@gmail.com>
2019-03-29 19:17:28 -0400

    Protocol3: split exchange code into multiple small libraries. (#58)

---
+++ packages/loopring_v3/contracts/impl/Exchange2.sol
@@ -0,0 +1,514 @@
+/*
+
+  Copyright 2017 Loopring Project Ltd (Loopring Foundation).
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+pragma solidity 0.5.2;
+
+import "../lib/Ownable.sol";
+
+import "../iface/ILoopringV3.sol";
+
+import "./libexchange/ExchangeAccounts.sol";
+import "./libexchange/ExchangeAdmins.sol";
+import "./libexchange/ExchangeBalances.sol";
+import "./libexchange/ExchangeBlocks.sol";
+import "./libexchange/ExchangeData.sol";
+import "./libexchange/ExchangeDeposits.sol";
+import "./libexchange/ExchangeGenesis.sol";
+import "./libexchange/ExchangeMode.sol";
+import "./libexchange/ExchangeTokens.sol";
+import "./libexchange/ExchangeWithdrawals.sol";
+
+
+/// @title An Implementation of IExchange.
+/// @author Brecht Devos - <brecht@loopring.org>
+/// @author Daniel Wang  - <daniel@loopring.org>
+contract Exchange2 is Ownable
+{
+    using ExchangeAdmins        for ExchangeData.State;
+    using ExchangeAccounts      for ExchangeData.State;
+    using ExchangeBalances      for ExchangeData.State;
+    using ExchangeBlocks        for ExchangeData.State;
+    using ExchangeDeposits      for ExchangeData.State;
+    using ExchangeGenesis       for ExchangeData.State;
+    using ExchangeMode          for ExchangeData.State;
+    using ExchangeTokens        for ExchangeData.State;
+    using ExchangeWithdrawals   for ExchangeData.State;
+
+    ExchangeData.State public state;
+
+    // -- Constructor --
+    constructor(
+        uint    _id,
+        address _loopring3Address,
+        address _owner,
+        address payable _operator
+        )
+        public
+        payable
+    {
+        require(address(0) != _owner, "ZERO_ADDRESS");
+        owner = _owner;
+
+        state.initializeGenesisBlock(
+            _id,
+            _loopring3Address,
+            _operator
+        );
+    }
+
+    modifier onlyOperator()
+    {
+        require(msg.sender == state.operator, "UNAUTHORIZED");
+        _;
+    }
+
+    // -- Mode --
+    function isInWithdrawalMode()
+        external
+        view
+        returns (bool result)
+    {
+        result = state.isInWithdrawalMode();
+    }
+
+    // -- Accounts --
+    function getAccount(
+        address owner
+        )
+        external
+        view
+        returns (
+            uint24 accountID,
+            uint   pubKeyX,
+            uint   pubKeyY
+        )
+    {
+        (accountID, pubKeyX, pubKeyY) = state.getAccount(owner);
+    }
+
+    function createOrUpdateAccount(
+        uint pubKeyX,
+        uint pubKeyY
+        )
+        external
+        payable
+        returns (uint24 accountID)
+    {
+        accountID = state.createOrUpdateAccount(pubKeyX, pubKeyY);
+    }
+
+    // -- Balances --
+    function isAccountBalanceCorrect(
+        uint256 merkleRoot,
+        uint24  accountID,
+        uint16  tokenID,
+        uint256 pubKeyX,
+        uint256 pubKeyY,
+        uint32  nonce,
+        uint96  balance,
+        uint256 tradeHistoryRoot,
+        uint256[24] calldata accountPath,
+        uint256[12] calldata balancePath
+        )
+        external
+        returns (bool)
+    {
+        return state.isAccountBalanceCorrect(
+            merkleRoot,
+            accountID,
+            tokenID,
+            pubKeyX,
+            pubKeyY,
+            nonce,
+            balance,
+            tradeHistoryRoot,
+            accountPath,
+            balancePath
+        );
+    }
+
+    // -- Tokens --
+    function registerToken(
+        address tokenAddress
+        )
+        external
+        payable
+        onlyOperator
+        returns (uint16 tokenID)
+    {
+        tokenID = state.registerToken(tokenAddress);
+    }
+
+    function getTokenID(
+        address tokenAddress
+        )
+        external
+        view
+        returns (uint16 tokenID)
+    {
+        tokenID = state.getTokenID(tokenAddress);
+    }
+
+    function getTokenAddress(
+        uint16 tokenID
+        )
+        external
+        view
+        returns (address tokenAddress)
+    {
+        tokenAddress = state.getTokenAddress(tokenID);
+    }
+
+    function disableTokenDeposit(
+        address tokenAddress
+        )
+        external
+        payable
+        onlyOperator
+    {
+        state.disableTokenDeposit(tokenAddress);
+    }
+
+    function enableTokenDeposit(
+        address tokenAddress
+        )
+        external
+        payable
+        onlyOperator
+    {
+        state.enableTokenDeposit(tokenAddress);
+    }
+
+    // -- Stakes --
+    function getStake()
+        external
+        view
+        returns (uint)
+    {
+        return state.loopring.getStake(state.id);
+    }
+
+    // -- Blocks --
+    function getBlockHeight()
+        external
+        view
+        returns (uint)
+    {
+        return state.blocks.length - 1;
+    }
+
+    function commitBlock(
+        uint  blockType,
+        bytes calldata data
+        )
+        external
+        payable
+        onlyOperator
+    {
+        state.commitBlock(blockType, data);
+    }
+
+    function verifyBlock(
+        uint blockIdx,
+        uint256[8] calldata proof
+        )
+        external
+        payable
+        onlyOperator
+    {
+        state.verifyBlock(blockIdx, proof);
+    }
+
+    function revertBlock(
+        uint32 blockIdx
+        )
+        external
+        payable
+        onlyOperator
+    {
+        state.revertBlock(blockIdx);
+    }
+
+    // -- Deposits --
+    function getFirstUnprocessedDepositRequestIndex()
+        external
+        view
+        returns (uint)
+    {
+        return state.getFirstUnprocessedDepositRequestIndex();
+    }
+
+    function getNumAvailableDepositSlots()
+        external
+        view
+        returns (uint)
+    {
+        // TODO
+        return 1024;
+    }
+
+    function getDepositRequest(
+        uint index
+        )
+        external
+        view
+        returns (
+          bytes32 accumulatedHash,
+          uint256 accumulatedFee,
+          uint32  timestamp
+        )
+    {
+        (accumulatedHash, accumulatedFee, timestamp) = state.getDepositRequest(index);
+    }
+
+    function deposit(
+        uint    pubKeyX,
+        uint    pubKeyY,
+        address token,
+        uint96  amount
+        )
+        external
+        payable
+        returns (uint24 accountID)
+    {
+        accountID = state.createOrUpdateAccount(pubKeyX, pubKeyY);
+        state.depositTo(msg.sender, token, amount);
+    }
+
+    function deposit(
+        address token,
+        uint96  amount
+        )
+        external
+        payable
+    {
+        state.depositTo(msg.sender, token, amount);
+    }
+
+    function depositTo(
+        address recipient,
+        address tokenAddress,
+        uint96  amount
+        )
+        external
+        payable
+    {
+        state.depositTo(recipient, tokenAddress, amount);
+    }
+
+    // -- Withdrawals --
+    function getFirstUnprocessedWithdrawalRequestIndex()
+        external
+        view
+        returns (uint)
+    {
+        return state.getFirstUnprocessedWithdrawalRequestIndex();
+    }
+
+    function getNumAvailableWithdrawalSlots(
+        )
+        external
+        view
+        returns (uint)
+    {
+        return state.getNumAvailableWithdrawalSlots();
+    }
+
+    function getWithdrawRequest(
+        uint index
+        )
+        external
+        view
+        returns (
+            bytes32 accumulatedHash,
+            uint256 accumulatedFee,
+            uint32 timestamp
+        )
+    {
+        (accumulatedHash, accumulatedFee, timestamp) = state.getWithdrawRequest(index);
+    }
+
+    // Set the large value for amount to withdraw the complete balance
+    function withdraw(
+        address token,
+        uint96 amount
+        )
+        external
+        payable
+    {
+        state.withdraw(token, amount);
+    }
+
+    function withdrawFromMerkleTree(
+        address token,
+        uint32  nonce,
+        uint96  balance,
+        uint256 tradeHistoryRoot,
+        uint256[24] calldata accountPath,
+        uint256[12] calldata balancePath
+        )
+        external
+        payable
+    {
+        state.withdrawFromMerkleTreeFor(
+            msg.sender,
+            token,
+            nonce,
+            balance,
+            tradeHistoryRoot,
+            accountPath,
+            balancePath
+        );
+    }
+
+    // We still alow anyone to withdraw these funds for the account owner
+    function withdrawFromMerkleTreeFor(
+        address owner,
+        address token,
+        uint32  nonce,
+        uint96  balance,
+        uint256 tradeHistoryRoot,
+        uint256[24] calldata accountPath,
+        uint256[12] calldata balancePath
+        )
+        external
+        payable
+    {
+        state.withdrawFromMerkleTreeFor(
+            owner,
+            token,
+            nonce,
+            balance,
+            tradeHistoryRoot,
+            accountPath,
+            balancePath
+        );
+    }
+
+    function withdrawFromDepositRequest(
+        uint depositRequestIdx
+        )
+        external
+        payable
+    {
+        state.withdrawFromDepositRequest(depositRequestIdx);
+    }
+
+    function withdrawFromApprovedWithdrawal(
+        uint blockIdx,
+        uint slotIdx
+        )
+        external
+        payable
+    {
+        state.withdrawFromApprovedWithdrawal(
+            blockIdx,
+            slotIdx
+        );
+    }
+
+    function withdrawBlockFee(
+        uint32 blockIdx
+        )
+        external
+        payable
+        returns (uint feeAmount)
+    {
+        feeAmount = state.withdrawBlockFee(blockIdx);
+    }
+
+    function distributeWithdrawals(
+        uint blockIdx
+        )
+        external
+        payable
+        onlyOperator
+    {
+        state.distributeWithdrawals(blockIdx);
+    }
+
+    // -- Admins --
+    function setOperator(
+        address payable _operator
+        )
+        external
+        onlyOwner
+        returns (address payable oldOperator)
+    {
+        oldOperator = state.setOperator(_operator);
+    }
+
+    function setFees(
+        uint _accountCreationFeeETH,
+        uint _accountUpdateFeeETH,
+        uint _depositFeeETH,
+        uint _withdrawalFeeETH
+        )
+        external
+        onlyOwner
+    {
+        state.setFees(
+            _accountCreationFeeETH,
+            _accountUpdateFeeETH,
+            _depositFeeETH,
+            _withdrawalFeeETH
+        );
+    }
+
+    function getFees()
+        external
+        view
+        returns (
+            uint _accountCreationFeeETH,
+            uint _accountUpdateFeeETH,
+            uint _depositFeeETH,
+            uint _withdrawalFeeETH
+        )
+    {
+        _accountCreationFeeETH = state.accountCreationFeeETH;
+        _accountUpdateFeeETH = state.accountUpdateFeeETH;
+        _depositFeeETH = state.depositFeeETH;
+        _withdrawalFeeETH = state.withdrawalFeeETH;
+    }
+
+    function purchaseDowntime(
+        uint durationSeconds
+        )
+        external
+        payable
+        onlyOperator
+    {
+        state.purchaseDowntime(durationSeconds);
+    }
+
+    function getRemainingDowntime()
+        external
+        view
+        returns (uint durationSeconds)
+    {
+        durationSeconds = state.getRemainingDowntime();
+    }
+
+    function getDowntimeCostLRC(
+        uint durationSeconds
+        )
+        external
+        view
+        returns (uint costLRC)
+    {
+        costLRC = state.getDowntimeCostLRC(durationSeconds);
+    }
+}
