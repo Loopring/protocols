8d87107013670d558c401b560a70c5126930c98f
kongliangzhong <kongliangzhong@gmail.com>
2019-04-09 11:04:25 +0800

    lrc_v2 token contract rename (#126)

--- packages/lrc_v2/migrations/2_deploy_token.js
+++ packages/lrc_v2/migrations/2_deploy_token.js
@@ -1,5 +1,5 @@
-var LRCToken = artifacts.require("./NewLRCToken.sol");
+var LRC_v2 = artifacts.require("./LRC_v2.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(LRCToken);
+  deployer.deploy(LRC_v2);
 };
