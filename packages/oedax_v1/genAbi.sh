37c67464b5e2fce58bf8e6cc78112244d1e8a5fd
Daniel Wang <daniel@loopring.org>
2019-05-01 22:01:18 -0400

    [Oedax] implement book keeping (part2)

--- packages/oedax_v1/genAbi.sh
+++ packages/oedax_v1/genAbi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-solc --abi contracts/iface/*.sol -o ABI/version30/ --overwrite
+solc --abi contracts/iface/*.sol -o ABI/version10/ --overwrite
 
-solc --abi contracts/lib/*.sol --allow-paths contracts/iface/*.sol -o ABI/version30/ --overwrite
+solc --abi contracts/lib/*.sol --allow-paths contracts/iface/*.sol -o ABI/version10/ --overwrite
