6e491ea18007dc689023e02c9d0af650cfa46e37
Daniel Wang <daniel@loopring.org>
2019-05-31 19:36:12 -0400

    [Protocol3] Implement a simple offchain data-availability approach

--- packages/loopring_v3/contracts/impl/Exchange.sol
+++ packages/loopring_v3/contracts/impl/Exchange.sol
@@ -369,13 +369,14 @@ contract Exchange is IExchange, Ownable, ReentrancyGuard
     function commitBlock(
         uint8 blockType,
         uint16 blockSize,
-        bytes calldata data
+        bytes calldata data,
+        bytes calldata offchainData
         )
         external
         onlyOperator
         nonReentrant
     {
-        state.commitBlock(blockType, blockSize, data);
+        state.commitBlock(blockType, blockSize, data, offchainData);
     }
 
     function verifyBlock(
