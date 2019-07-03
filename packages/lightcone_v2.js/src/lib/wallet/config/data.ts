fd5e2343197cf4dc01a68cc7d01e7810cf9e70a3
SunnyWheat <eth_dev@126.com>
2019-07-02 20:52:37 -0700

    [Lightcone_V2.js]  Make lightcone_v2.js to be imported in react web app (#243)
    * Point DexService to aws hosted service
    
    * Make lightcone_v2.js to import in react web app
    
    * Update grpcClient.ts
    
    * [Lightcone_V2.js] Migrate Lightcone v2 Javascript to Typescript (#244)
    
    * Migrate lightcone v2 js to typescript (Step 1)
    
    * Solve typesript errors in walletAccount.ts
    
    * Resolve all errors during migration from Javascript to Typescript.
    
    * Move tests to typescript in lightcone_v2.js
    
    * Add resolveJsonModule to true. Update index.js which is the entrypoint of the package.
    
    * Add GetNextOrderIdReq to test importing grpc . Reenable test/grpc.ts
    
    * Update grpcClient

---
+++ packages/lightcone_v2.js/src/lib/wallet/config/data.ts
@@ -0,0 +1,9 @@
+import tokens from './tokens'
+
+export {tokens};
+
+export const configs = require('./config.json.js');
+export const paths = require('./path.json.js');
+export const wallets = require('./dpath.json.js');
+export const projects = require('./projects.json.js');
+
