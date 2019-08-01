82be0c3e633e98cd1367d2bb03d6bae68e65b8f0
Daniel Wang <daniel@loopring.org>
2019-07-31 21:48:45 -0400

    more update

--- packages/loopring_v3/contracts/iface/IUserStakingPool.sol
+++ packages/loopring_v3/contracts/iface/IUserStakingPool.sol
@@ -14,7 +14,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-pragma solidity 0.5.7;
+pragma solidity 0.5.10;
 
 
 /// @title IUserStakingPool
@@ -52,15 +52,15 @@ contract IUserStakingPool
     /// @param user The user address.
     /// @return withdrawalWaitTime Time in seconds that the user has to wait before any LRC can be withdrawn.
     /// @return rewardWaitTime Time in seconds that the user has to wait before any LRC reward can be claimed.
-    /// @return stakeAmount The amount of LRC staked.
-    /// @return claimableReward The amount of LRC reward waiting to be claimed.
+    /// @return balance The amount of LRC staked or rewarded.
+    /// @return claimableReward The amount of LRC reward claimable.
     function getUserStaking(address user)
         external
         view
         returns (
             uint withdrawalWaitTime,
             uint rewardWaitTime,
-            uint stakeAmount,
+            uint balance,
             uint claimableReward
         );
 
