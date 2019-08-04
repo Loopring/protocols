0fc24f51a342f943f6cb3ed2324a2ed6c9f31975
Daniel Wang <daniel@loopring.org>
2019-08-04 01:28:03 -0400

    fix a bug

--- packages/loopring_v3/contracts/impl/auxiliaries/IDynamicDowntimePriceProvider.sol
+++ packages/loopring_v3/contracts/impl/auxiliaries/IDynamicDowntimePriceProvider.sol
@@ -24,7 +24,7 @@ import "../../iface/IDowntimePriceProvider.sol";
 
 /// @title An simple implementation of IDowntimePriceProvider.
 /// @author Daniel Wang  - <daniel@loopring.org>
-/// TODO(danniel): re-implement this based on feedback from Brecht.
+/// TODO(daniel): re-implement this based on feedback from Brecht.
 contract IDynamicDowntimePriceProvider is IDowntimePriceProvider, Claimable
 {
     using MathUint for uint;
