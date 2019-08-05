03676f686dbd6eedeb8b2763a0929f975eecbd62
Daniel Wang <daniel@loopring.org>
2019-08-05 16:57:30 -0400

    add ProtocolRegistry

---
+++ packages/loopring_v3/contracts/iface/IProtocolRegistry.sol
@@ -0,0 +1,61 @@
+/*
+
+  Copyright 2017 Loopring Project Ltd (Loopring Foundation).
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+pragma solidity 0.5.10;
+
+
+/// @title IProtocalRegistry
+/// @author Daniel Wang  - <daniel@loopring.org>
+contract IProtocolRegistry {
+
+    // function getDefaultProtocol()
+    //     external
+    //     view
+    //     returns (
+    //         address loopring,
+    //         address instance,
+    //         string  version
+    //     );
+
+    // function setDefaultProtocol(address protocol)
+    //     external;
+
+    function getProtocol(
+        address loopring
+        )
+        public
+        view
+        returns (
+            address instance,
+            string memory version
+        );
+
+    function registerProtocol(
+        address loopring,
+        address instance,
+        string  memory version
+        )
+        public;
+
+    function createExchange(
+        address loopring
+        )
+        public
+        returns (
+            address exchangeProxy,
+            uint    exchangeId
+        );
+}
---
+++ packages/loopring_v3/contracts/impl/ProtocolRegistry.sol
@@ -0,0 +1,80 @@
+/*
+
+  Copyright 2017 Loopring Project Ltd (Loopring Foundation).
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+pragma solidity 0.5.10;
+
+import "../lib/Claimable.sol";
+
+import "../iface/IProtocolRegistry.sol";
+
+
+/// @title An Implementation of IProtocolRegistry.
+/// @author Daniel Wang  - <daniel@loopring.org>
+contract ProtocolRegistry is IProtocolRegistry, Claimable {
+    struct Protocol
+    {
+       address instance;
+       string  version;
+    }
+
+    mapping (address => Protocol) private protocols;
+
+    constructor() Claimable() public {}
+
+    function getProtocol(
+        address loopring
+        )
+        public
+        view
+        returns (
+            address instance,
+            string  memory version
+        )
+    {
+        Protocol storage protocol = protocols[loopring];
+        instance = protocol.instance;
+        version = protocol.version;
+        require(instance != address(0), "INVALID_PROTOCOL");
+    }
+
+    function registerProtocol(
+        address loopring,
+        address instance,
+        string  memory version
+        )
+        public
+    {
+        require(loopring != address(0), "ZERO_ADDRESS");
+        require(instance != address(0), "ZERO_ADDRESS");
+        require(bytes(version).length > 0, "INVALID_VERSION_LABEL");
+        protocols[loopring] = Protocol(instance, version);
+    }
+
+    function createExchange(
+        address loopring
+        )
+        public
+        returns (
+            address exchangeProxy,
+            uint    exchangeId
+        )
+    {
+        getProtocol(loopring); // verifies the input
+        // ExchangeProxy proxy = new ExchangeProxy(address(this), loopring);
+        // exchangeProxy = address(proxy);
+        // exchangeId = ILoopring(loopring).registerProxy(exchangeProxy);
+    }
+}
