bfa4af5e9d84b7140614a31c2f296e6c0813f379
Daniel Wang <daniel@loopring.org>
2019-08-07 12:36:11 -0400

    fix testExchangeTokens

--- packages/loopring_v3/test/testExchangeTokens.ts
+++ packages/loopring_v3/test/testExchangeTokens.ts
@@ -75,148 +75,148 @@ contract("Exchange", (accounts: string[]) => {
   };
 
   const createExchange = async (bSetupTestState: boolean = true) => {
-    // exchangeID = await exchangeTestUtil.createExchange(
-    //   exchangeTestUtil.testContext.stateOwners[0],
-    //   bSetupTestState
-    // );
-    // exchange = exchangeTestUtil.exchange;
+    exchangeID = await exchangeTestUtil.createExchange(
+      exchangeTestUtil.testContext.stateOwners[0],
+      bSetupTestState
+    );
+    exchange = exchangeTestUtil.exchange;
   };
 
   before(async () => {
     exchangeTestUtil = new ExchangeTestUtil();
     await exchangeTestUtil.initialize(accounts);
-    // exchange = exchangeTestUtil.exchange;
-    // loopring = exchangeTestUtil.loopringV3;
-    // exchangeID = 1;
+    exchange = exchangeTestUtil.exchange;
+    loopring = exchangeTestUtil.loopringV3;
+    exchangeID = 1;
   });
 
   describe("Tokens", function() {
     this.timeout(0);
 
-    // describe("exchange owner", () => {
-    //   it("should be able to register a token", async () => {
-    //     await createExchange(false);
+    describe("exchange owner", () => {
+      it("should be able to register a token", async () => {
+        await createExchange(false);
 
-    //     // Make sure the exchange owner has enough LRC
-    //     const registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
-    //     await exchangeTestUtil.setBalanceAndApprove(
-    //       exchangeTestUtil.exchangeOwner,
-    //       "LRC",
-    //       registrationCost
-    //     );
+        // Make sure the exchange owner has enough LRC
+        const registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
+        await exchangeTestUtil.setBalanceAndApprove(
+          exchangeTestUtil.exchangeOwner,
+          "LRC",
+          registrationCost
+        );
 
-    //     // Register the token
-    //     await registerTokenChecked("GTO", exchangeTestUtil.exchangeOwner);
-    //   });
+        // Register the token
+        await registerTokenChecked("GTO", exchangeTestUtil.exchangeOwner);
+      });
 
-    //   it("should not be able to register a token multiple times", async () => {
-    //     await createExchange(false);
+      it("should not be able to register a token multiple times", async () => {
+        await createExchange(false);
 
-    //     // Make sure the exchange owner has enough LRC
-    //     const registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
-    //     await exchangeTestUtil.setBalanceAndApprove(
-    //       exchangeTestUtil.exchangeOwner,
-    //       "LRC",
-    //       registrationCost
-    //     );
+        // Make sure the exchange owner has enough LRC
+        const registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
+        await exchangeTestUtil.setBalanceAndApprove(
+          exchangeTestUtil.exchangeOwner,
+          "LRC",
+          registrationCost
+        );
 
-    //     // Register the token
-    //     await registerTokenChecked("GTO", exchangeTestUtil.exchangeOwner);
+        // Register the token
+        await registerTokenChecked("GTO", exchangeTestUtil.exchangeOwner);
 
-    //     // Try to register the token again
-    //     await expectThrow(
-    //       exchange.registerToken(exchangeTestUtil.getTokenAddress("GTO"), {
-    //         from: exchangeTestUtil.exchangeOwner
-    //       }),
-    //       "TOKEN_ALREADY_EXIST"
-    //     );
-    //   });
-    // });
+        // Try to register the token again
+        await expectThrow(
+          exchange.registerToken(exchangeTestUtil.getTokenAddress("GTO"), {
+            from: exchangeTestUtil.exchangeOwner
+          }),
+          "TOKEN_ALREADY_EXIST"
+        );
+      });
+    });
 
     describe("anyone", () => {
       it("should not be able to register a token", async () => {
         await createExchange(false);
-        // const token = exchangeTestUtil.getTokenAddress("GTO");
-        // await expectThrow(
-        //   exchange.registerToken(token, {
-        //     from: exchangeTestUtil.testContext.orderOwners[0]
-        //   }),
-        //   "UNAUTHORIZED"
-        // );
+        const token = exchangeTestUtil.getTokenAddress("GTO");
+        await expectThrow(
+          exchange.registerToken(token, {
+            from: exchangeTestUtil.testContext.orderOwners[0]
+          }),
+          "UNAUTHORIZED"
+        );
       });
 
-      // it("should not be able to get the token ID for an unregistered token", async () => {
-      //   await createExchange(false);
-      //   const token = exchangeTestUtil.getTokenAddress("GTO");
-      //   await expectThrow(exchange.getTokenID(token), "TOKEN_NOT_FOUND");
-      // });
+      it("should not be able to get the token ID for an unregistered token", async () => {
+        await createExchange(false);
+        const token = exchangeTestUtil.getTokenAddress("GTO");
+        await expectThrow(exchange.getTokenID(token), "TOKEN_NOT_FOUND");
+      });
 
-      // it("should not be able to get the token address for an invalid token ID", async () => {
-      //   await createExchange(false);
-      //   await expectThrow(exchange.getTokenAddress(123), "INVALID_TOKEN_ID");
-      // });
+      it("should not be able to get the token address for an invalid token ID", async () => {
+        await createExchange(false);
+        await expectThrow(exchange.getTokenAddress(123), "INVALID_TOKEN_ID");
+      });
     });
 
-    // it("token registration cost should be as expected", async () => {
-    //   await createExchange(false);
+    it("token registration cost should be as expected", async () => {
+      await createExchange(false);
 
-    //   let numTokens = 3;
-    //   let expectedCost = await getTokenRegistrationCost(numTokens);
-    //   let registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
-    //   assert(
-    //     registrationCost.eq(expectedCost),
-    //     "token registration cost not as expected"
-    //   );
+      let numTokens = 3;
+      let expectedCost = await getTokenRegistrationCost(numTokens);
+      let registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
+      assert(
+        registrationCost.eq(expectedCost),
+        "token registration cost not as expected"
+      );
 
-    //   // Register the token
-    //   await exchangeTestUtil.setBalanceAndApprove(
-    //     exchangeTestUtil.exchangeOwner,
-    //     "LRC",
-    //     registrationCost
-    //   );
-    //   await registerTokenChecked("GTO", exchangeTestUtil.exchangeOwner);
-    //   numTokens++;
+      // Register the token
+      await exchangeTestUtil.setBalanceAndApprove(
+        exchangeTestUtil.exchangeOwner,
+        "LRC",
+        registrationCost
+      );
+      await registerTokenChecked("GTO", exchangeTestUtil.exchangeOwner);
+      numTokens++;
 
-    //   expectedCost = await getTokenRegistrationCost(numTokens);
-    //   registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
-    //   assert(
-    //     registrationCost.eq(expectedCost),
-    //     "token registration cost not as expected"
-    //   );
-    // });
+      expectedCost = await getTokenRegistrationCost(numTokens);
+      registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
+      assert(
+        registrationCost.eq(expectedCost),
+        "token registration cost not as expected"
+      );
+    });
 
-    // it("LRC, ETH and WETH should be preregistered", async () => {
-    //   await createExchange(false);
+    it("LRC, ETH and WETH should be preregistered", async () => {
+      await createExchange(false);
 
-    //   // Make sure the exchange owner has enough LRC
-    //   const registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
-    //   await exchangeTestUtil.setBalanceAndApprove(
-    //     exchangeTestUtil.exchangeOwner,
-    //     "LRC",
-    //     registrationCost
-    //   );
+      // Make sure the exchange owner has enough LRC
+      const registrationCost = await exchange.getLRCFeeForRegisteringOneMoreToken();
+      await exchangeTestUtil.setBalanceAndApprove(
+        exchangeTestUtil.exchangeOwner,
+        "LRC",
+        registrationCost
+      );
 
-    //   // Try to register LRC
-    //   await expectThrow(
-    //     exchange.registerToken(exchangeTestUtil.getTokenAddress("LRC"), {
-    //       from: exchangeTestUtil.exchangeOwner
-    //     }),
-    //     "TOKEN_ALREADY_EXIST"
-    //   );
-    //   // Try to register ETH
-    //   await expectThrow(
-    //     exchange.registerToken(exchangeTestUtil.getTokenAddress("ETH"), {
-    //       from: exchangeTestUtil.exchangeOwner
-    //     }),
-    //     "TOKEN_ALREADY_EXIST"
-    //   );
-    //   // Try to register WETH
-    //   await expectThrow(
-    //     exchange.registerToken(exchangeTestUtil.getTokenAddress("WETH"), {
-    //       from: exchangeTestUtil.exchangeOwner
-    //     }),
-    //     "TOKEN_ALREADY_EXIST"
-    //   );
-    // });
+      // Try to register LRC
+      await expectThrow(
+        exchange.registerToken(exchangeTestUtil.getTokenAddress("LRC"), {
+          from: exchangeTestUtil.exchangeOwner
+        }),
+        "TOKEN_ALREADY_EXIST"
+      );
+      // Try to register ETH
+      await expectThrow(
+        exchange.registerToken(exchangeTestUtil.getTokenAddress("ETH"), {
+          from: exchangeTestUtil.exchangeOwner
+        }),
+        "TOKEN_ALREADY_EXIST"
+      );
+      // Try to register WETH
+      await expectThrow(
+        exchange.registerToken(exchangeTestUtil.getTokenAddress("WETH"), {
+          from: exchangeTestUtil.exchangeOwner
+        }),
+        "TOKEN_ALREADY_EXIST"
+      );
+    });
   });
 });
