bc0cdde15195e4048fde79401bec3f0b9f97f27c
Daniel Wang <daniel@loopring.org>
2019-08-08 17:59:20 -0400

    more

--- packages/loopring_v3/test/testExchangeUtil.ts
+++ packages/loopring_v3/test/testExchangeUtil.ts
@@ -3708,8 +3708,6 @@ export class ExchangeTestUtil {
     this.lrcAddress = lrcToken.address;
     this.wethAddress = wethToken.address;
 
-    await protocolRegistry.setLRCAddress(lrcToken.address);
-
     const currBlockNumber = await web3.eth.getBlockNumber();
     const currBlockTimestamp = (await web3.eth.getBlock(currBlockNumber))
       .timestamp;
