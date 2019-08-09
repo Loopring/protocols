bb71c0c44306061206443bb64271f8f023ec27ec
Daniel Wang <daniel@loopring.org>
2019-08-08 21:39:09 -0400

    rename IExchange => IExchangeV3, Exchange => ExchangeV3

--- packages/loopring_v3/contracts/impl/LoopringV3.sol
+++ packages/loopring_v3/contracts/impl/LoopringV3.sol
@@ -16,8 +16,8 @@
 */
 pragma solidity 0.5.10;
 
-import "../iface/ILoopringV3.sol";
 import "../iface/IExchangeV3.sol";
+import "../iface/ILoopringV3.sol";
 
 import "../lib/AddressUtil.sol";
 import "../lib/BurnableERC20.sol";
