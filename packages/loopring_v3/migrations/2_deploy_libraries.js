10e1c24c8b44ac76efef6fdc4d767e0402913a85
Daniel Wang <daniel@loopring.org>
2019-08-10 19:15:11 -0400

    fix out of gas deployment error

--- packages/loopring_v3/migrations/2_deploy_libraries.js
+++ packages/loopring_v3/migrations/2_deploy_libraries.js
@@ -1,29 +1,30 @@
-// Deploy all libraries with public methods that are shared by/linked from other contracts.
+// Deploy libraries with public methods that are shared by/linked from other contracts.
+// Libraries with only privat eor internal methods should not be deployed independently.
 // The purpose of deploying these libraries stand-alone is to reduce gas usage in deploying
 /// other contracts.
 
-var AddressUtil = artifacts.require("./lib/AddressUtil.sol");
-var ERC20SafeTransfer = artifacts.require("./lib/ERC20SafeTransfer.sol");
-var MathUint = artifacts.require("./lib/MathUint.sol");
+// var AddressUtil = artifacts.require("./lib/AddressUtil.sol");
+// var ERC20SafeTransfer = artifacts.require("./lib/ERC20SafeTransfer.sol");
+// var MathUint = artifacts.require("./lib/MathUint.sol");
 
 module.exports = function(deployer, network, accounts) {
   if (network === "live") {
     // ignore.
   } else {
-    deployer
-      .then(() => {
-        return Promise.all([
-          deployer.deploy(AddressUtil),
-          deployer.deploy(ERC20SafeTransfer),
-          deployer.deploy(MathUint)
-        ]);
-      })
-      .then(() => {
-        console.log(">>>>>>>> contracts deployed by deploy_libs:");
-        console.log("AddressUtil:", AddressUtil.address);
-        console.log("ERC20SafeTransfer:", ERC20SafeTransfer.address);
-        console.log("MathUint:", MathUint.address);
-        console.log("");
-      });
+    // deployer
+    //   .then(() => {
+    //     return Promise.all([
+    //       deployer.deploy(AddressUtil),
+    //       deployer.deploy(ERC20SafeTransfer),
+    //       deployer.deploy(MathUint)
+    //     ]);
+    //   })
+    //   .then(() => {
+    //     console.log(">>>>>>>> contracts deployed by deploy_libs:");
+    //     console.log("AddressUtil:", AddressUtil.address);
+    //     console.log("ERC20SafeTransfer:", ERC20SafeTransfer.address);
+    //     console.log("MathUint:", MathUint.address);
+    //     console.log("");
+    //   });
   }
 };
