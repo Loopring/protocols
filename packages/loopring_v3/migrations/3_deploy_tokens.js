eafd213f4fabb93b458192db4214fc4e64299b44
Daniel Wang <daniel@loopring.org>
2019-08-12 21:30:13 -0400

    more

--- packages/loopring_v3/migrations/3_deploy_tokens.js
+++ packages/loopring_v3/migrations/3_deploy_tokens.js
@@ -1,18 +1,15 @@
 // Deploy tokens for testing purposes
 
-var LRCToken = artifacts.require("./test/tokens/LRC.sol");
-var GTOToken = artifacts.require("./test/tokens/GTO.sol");
-var RDNToken = artifacts.require("./test/tokens/RDN.sol");
-var REPToken = artifacts.require("./test/tokens/REP.sol");
-var WETHToken = artifacts.require("./test/tokens/WETH.sol");
-var TESTToken = artifacts.require("./test/tokens/TEST.sol");
-var INDAToken = artifacts.require("./test/tokens/INDA.sol");
-var INDBToken = artifacts.require("./test/tokens/INDB.sol");
-
 module.exports = function(deployer, network, accounts) {
-  if (network === "live") {
-    // ignore
-  } else {
+  if (network !== "live") {
+    const LRCToken = artifacts.require("./test/tokens/LRC.sol");
+    const GTOToken = artifacts.require("./test/tokens/GTO.sol");
+    const RDNToken = artifacts.require("./test/tokens/RDN.sol");
+    const REPToken = artifacts.require("./test/tokens/REP.sol");
+    const WETHToken = artifacts.require("./test/tokens/WETH.sol");
+    const TESTToken = artifacts.require("./test/tokens/TEST.sol");
+    const INDAToken = artifacts.require("./test/tokens/INDA.sol");
+    const INDBToken = artifacts.require("./test/tokens/INDB.sol");
     deployer.deploy(LRCToken);
     deployer.deploy(GTOToken);
     deployer.deploy(RDNToken);
