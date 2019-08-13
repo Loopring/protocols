d0b9baddfff8032ffeb5611aebf21113daa1a96d
Daniel Wang <daniel@loopring.org>
2019-08-13 12:35:58 -0400

    deployable

--- packages/loopring_v3/contracts/impl/libexchange/ExchangeBlocks.sol
+++ packages/loopring_v3/contracts/impl/libexchange/ExchangeBlocks.sol
@@ -67,7 +67,7 @@ library ExchangeBlocks
         bytes  memory data,
         bytes  memory /*offchainData*/
         )
-        internal
+        public
     {
         commitBlockInternal(
             S,
