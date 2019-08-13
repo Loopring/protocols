ffb924ef8e5d671aec7a358b2315a25368176de9
Daniel Wang <daniel@loopring.org>
2019-08-13 00:29:50 -0400

    more

--- packages/loopring_v3/migrations/1_initial_migration.js
+++ packages/loopring_v3/migrations/1_initial_migration.js
@@ -1,5 +1,14 @@
 var Migrations = artifacts.require("./Migrations");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function(deployer, network, accounts) {
+  console.log("   > deploying to network: " + network);
+  deployer
+    .then(() => {
+      return Promise.all([deploy(Migrations)]);
+    })
+    .then(() => {
+      console.log(">>>>>>>> contracts deployed by deploy_registry:");
+      console.log("Migrations:", Migrations.address);
+      console.log("");
+    });
 };
