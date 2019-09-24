72efe633956320ed79b74eca1e17576afd87af54
kenshin <kenshin_6@163.com>
2019-09-24 12:20:41 +0800

    add config exporting

--- packages/lightcone_v2.js/src/index.ts
+++ packages/lightcone_v2.js/src/index.ts
@@ -3,12 +3,14 @@ import ethereum from "./lib/wallet/ethereum";
 import ContractUtils from "./lib/wallet/ethereum/contracts/Contracts";
 import EthRpcUtils from "./lib/wallet/ethereum/eth";
 import Utils from "./lib/wallet/common/utils";
+import config from "./lib/wallet/config";
 
 import { exchange } from "./sign/exchange";
 import { Account } from "./wallet/account";
 
 export {
   common,
+  config,
   ethereum,
   exchange,
   Account,
