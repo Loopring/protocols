9bff2a5ce5d84b8840dfc173ff2206853af085dd
Daniel Wang <daniel@loopring.org>
2019-09-28 19:59:37 -0400

    implement UniswapTokenSeller

--- packages/loopring_v3/migrations/5_deploy_auxiliaries.js
+++ packages/loopring_v3/migrations/5_deploy_auxiliaries.js
@@ -5,10 +5,12 @@ var DowntimeCostCalculator = artifacts.require(
   "./impl/DowntimeCostCalculator.sol"
 );
 
+var UniswapTokenSeller = artifacts.require("./impl/UniswapTokenSeller.sol");
 var lrcAddress = "0xBBbbCA6A901c926F240b89EacB641d8Aec7AEafD";
 var wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 var protocolFeeValutAddress = "0xa8b6A3EFBcdd578154a913F33dc9949808B7A9f4";
 var userStakingPoolAddress = "[undeployed]";
+var uniswapTokenSellerAddress = "[undeployed]";
 
 module.exports = function(deployer, network, accounts) {
   console.log("deploying to network: " + network);
@@ -53,8 +55,22 @@ module.exports = function(deployer, network, accounts) {
       });
   }
 
-  // common deployment
-  // common deployment
+  if (network === "live" || network === "rinkeby") {
+    const factoryAddress =
+      network === "live"
+        ? "0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95"
+        : "0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36";
+
+    deployer_.then(() => {
+      return Promise.all([
+        deployer.deploy(
+          UniswapTokenSeller,
+          factoryAddress,
+          ProtocolFeeVault.deployed()
+        )
+      ]);
+    });
+  }
 
   const BatchVerifier = artifacts.require("./thirdparty/BatchVerifier.sol");
   const BlockVerifier = artifacts.require("./impl/BlockVerifier.sol");
@@ -78,6 +94,7 @@ module.exports = function(deployer, network, accounts) {
       console.log("wethAddress:", wethAddress);
       console.log("protocolFeeValutAddress:", protocolFeeValutAddress);
       console.log("userStakingPoolAddress:", userStakingPoolAddress);
+      console.log("uniswapTokenSellerAddress:", uniswapTokenSellerAddress);
       console.log("BlockVerifier:", BlockVerifier.address);
       console.log("DowntimeCostCalculator:", DowntimeCostCalculator.address);
       console.log("");
