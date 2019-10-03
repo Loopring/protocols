4144f570507ea7ff51cbbd82e620299c326f6cdd
Daniel Wang <daniel@loopring.org>
2019-10-03 11:25:11 -0400

    more

--- packages/loopring_v3/test/testUserStakingPool2.ts
+++ packages/loopring_v3/test/testUserStakingPool2.ts
@@ -41,81 +41,70 @@ contract("UserStakingPool2", (accounts: string[]) => {
     const alice = accounts[1];
     const bob = accounts[2];
 
-    console.error("alice", alice);
-    console.error("bob", bob);
+    const amount = new BN(web3.utils.toWei("100", "ether"));
+    var totalReward = new BN(web3.utils.toWei("500", "ether"));
 
-    it("Alice and bob stake the same amount and split reward", async () => {
-      const amount = new BN(web3.utils.toWei("100", "ether"));
-      {
-        await userStakingPool.stake(amount, { from: alice });
-        await userStakingPool.stake(amount, { from: bob });
+    it("Alice and bob stake the same amount but cannot claim before waiting period", async () => {
+      await userStakingPool.stake(amount, { from: alice });
+      await userStakingPool.stake(amount, { from: bob });
 
-        await advanceTimeAndBlockAsync(MIN_CLAIM_DELAY - 1);
+      await advanceTimeAndBlockAsync(MIN_CLAIM_DELAY - 1);
 
-        // cannot claim yet
-        await expectThrow(
-          userStakingPool.claim({ from: alice }),
-          "NEED_TO_WAIT"
-        );
-      }
+      // cannot claim yet
+      await expectThrow(userStakingPool.claim({ from: alice }), "NEED_TO_WAIT");
+    });
+    // mocke ProtocolFeeVault to return 500 LRC as total reward
 
-      // mocke ProtocolFeeVault to return 500 LRC as total reward
-
-      var totalReward = new BN(web3.utils.toWei("500", "ether"));
-
+    it("then Alice can claim 50% of the LRC reward", async () => {
       const getProtocolFeeStats = web3.utils
         .sha3("getProtocolFeeStats()")
         .slice(0, 10);
 
-      {
-        await mockProtocolFeeVault.givenMethodReturn(
-          getProtocolFeeStats,
-          abi.rawEncode(
-            ["uint", "uint", "uint", "uint", "uint", "uint", "uint", "uint"],
-            [0, 0, 0, 0, 0, 0, 0, totalReward]
-          )
-        );
+      await mockProtocolFeeVault.givenMethodReturn(
+        getProtocolFeeStats,
+        abi.rawEncode(
+          ["uint", "uint", "uint", "uint", "uint", "uint", "uint", "uint"],
+          [0, 0, 0, 0, 0, 0, 0, totalReward]
+        )
+      );
 
-        await advanceTimeAndBlockAsync(1);
+      await advanceTimeAndBlockAsync(1);
 
-        const tx = await userStakingPool.claim({ from: alice });
+      const tx = await userStakingPool.claim({ from: alice });
 
-        truffleAssert.eventEmitted(tx, "LRCRewarded", (evt: any) => {
-          return (
-            alice === evt.user && totalReward.div(new BN(2)).eq(evt.amount)
-          );
-        });
+      truffleAssert.eventEmitted(tx, "LRCRewarded", (evt: any) => {
+        return alice === evt.user && totalReward.div(new BN(2)).eq(evt.amount);
+      });
 
-        const {
-          0: withdrawalWaitTime,
-          1: rewardWaitTime,
-          2: balance,
-          3: claimableReward
-        } = await userStakingPool.getUserStaking(alice);
+      const {
+        0: withdrawalWaitTime,
+        1: rewardWaitTime,
+        2: balance,
+        3: claimableReward
+      } = await userStakingPool.getUserStaking(alice);
 
-        assert(rewardWaitTime.eq(new BN(MIN_CLAIM_DELAY)), "rewardWaitTime");
-        assert(balance.eq(amount.add(totalReward.div(new BN(2)))), "balance");
-        assert(claimableReward.eq(ZERO), "claimableReward");
-      }
+      assert(rewardWaitTime.eq(new BN(MIN_CLAIM_DELAY)), "rewardWaitTime");
+      assert(balance.eq(amount.add(totalReward.div(new BN(2)))), "balance");
+      assert(claimableReward.eq(ZERO), "claimableReward");
+    });
 
-      {
-        // then Bob can claim all remaining reward after timeout
-        const tx = await userStakingPool.claim({ from: bob });
-        truffleAssert.eventEmitted(tx, "LRCRewarded", (evt: any) => {
-          return bob === evt.user && totalReward.eq(evt.amount);
-        });
+    it("then Bob can claim 100% of the remaining LRC reward", async () => {
+      // then Bob can claim all remaining reward after timeout
+      const tx = await userStakingPool.claim({ from: bob });
+      truffleAssert.eventEmitted(tx, "LRCRewarded", (evt: any) => {
+        return bob === evt.user && totalReward.eq(evt.amount);
+      });
 
-        const {
-          0: withdrawalWaitTime,
-          1: rewardWaitTime,
-          2: balance,
-          3: claimableReward
-        } = await userStakingPool.getUserStaking(bob);
+      const {
+        0: withdrawalWaitTime,
+        1: rewardWaitTime,
+        2: balance,
+        3: claimableReward
+      } = await userStakingPool.getUserStaking(bob);
 
-        assert(rewardWaitTime.eq(new BN(MIN_CLAIM_DELAY)), "rewardWaitTime");
-        assert(balance.eq(amount.add(totalReward)), "balance");
-        assert(claimableReward.eq(ZERO), "claimableReward");
-      }
+      assert(rewardWaitTime.eq(new BN(MIN_CLAIM_DELAY)), "rewardWaitTime");
+      assert(balance.eq(amount.add(totalReward)), "balance");
+      assert(claimableReward.eq(ZERO), "claimableReward");
     });
   });
 });
