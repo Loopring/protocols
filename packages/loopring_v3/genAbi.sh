629299a1314c67c35a2cc572dfcbab1fdbaaa350
kongliangzhong <kongliangzhong@gmail.com>
2019-10-11 12:15:01 +0800

    update genAbi.sh

--- packages/loopring_v3/genAbi.sh
+++ packages/loopring_v3/genAbi.sh
@@ -16,11 +16,22 @@ node_modules/solc/solcjs \
     --abi contracts/iface/*.sol \
     --allow-paths contracts/thirdparty/*.sol contracts/lib/*.sol
 
+node_modules/solc/solcjs \
+    -o ABI/version30/ --overwrite \
+    --abi contracts/test/DummyToken.sol \
+    --allow-paths contracts/test/*.sol contracts/lib/*.sol contracts/thirdparty/*.sol contracts/iface/*.sol
+
+node_modules/solc/solcjs \
+    -o ABI/version30/ --overwrite \
+    --abi contracts/impl/libexchange/ExchangeConstants.sol \
+    contracts/impl/libexchange/ExchangeData.sol \
+    --allow-paths contracts/iface/*.sol contracts/lib/*.sol contracts/thirdparty/*.sol
+
 ABI_PATH="ABI/version30"
 
 for file in $ABI_PATH/*
 do
-    rename_file=$(echo $file | awk '{split($0,a,"_"); print a[5]}')
+    rename_file=$(echo "${file##*_}")
     mv $file $ABI_PATH/$rename_file
 done
 
