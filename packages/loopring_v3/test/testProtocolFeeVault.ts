6dd00895119c220c7a070a828ca1f3f2768aff42
AdamChen <adamchen0510@gmail.com>
2019-10-16 16:59:12 +0800

    Add more tests

--- packages/loopring_v3/test/testProtocolFeeVault.ts
+++ packages/loopring_v3/test/testProtocolFeeVault.ts
@@ -9,9 +9,10 @@ const abi = require("ethereumjs-abi");
 // Make sure the amount difference is no more than 0.001%;
 const isAmountCloseEnough = (bn1: BN, bn2: BN) => {
   const result =
-    bn1.lte(bn2) &&
+    // bn1.lte(bn2) &&
     bn2
       .sub(bn1)
+      .abs()
       .mul(new BN(100000))
       .div(bn2)
       .lte(new BN(1));
@@ -27,7 +28,8 @@ contract("ProtocolFeeVault", (accounts: string[]) => {
   const ProtocolFeeVault = contracts.ProtocolFeeVault;
 
   const ZERO = new BN(0);
-  const amount = new BN(web3.utils.toWei("100", "ether"));
+  const amount = new BN(web3.utils.toWei("77", "ether"));
+  const amount2 = new BN(web3.utils.toWei("100", "ether"));
 
   var mockLRC: any;
   var mockToken: any;
@@ -80,28 +82,13 @@ contract("ProtocolFeeVault", (accounts: string[]) => {
       });
     });
 
-    describe("claimStakingReward", () => {
-      // it("claimStakingReward need called by userStakingPoolAddress", async () => {
-      //   await expectThrow(
-      //     protocolFeeVault.claimStakingReward(0),
-      //     "ZERO_VALUE"
-      //   );
-      //   await expectThrow(
-      //     protocolFeeVault.claimStakingReward(amount),
-      //     "UNAUTHORIZED"
-      //   );
-      //   await protocolFeeVault.claimStakingReward(amount, {from: userStakingPoolAddress})
-      // });
-    });
-
-    describe("fundDao", () => {
-      it("fundDao when thers is no fee", async () => {
+    describe("fundDao and check fee stats", () => {
+      it("fundDao when there is no fee", async () => {
         const feeBalance = ZERO;
         // mock lrc to return 0 either
         const getBalanceOfFee = web3.utils
           .sha3("balanceOf(address)")
           .slice(0, 10);
-
         await mockLRC.givenMethodReturn(
           getBalanceOfFee,
           abi.rawEncode(["uint"], [feeBalance])
@@ -127,11 +114,10 @@ contract("ProtocolFeeVault", (accounts: string[]) => {
 
       it("fundDao when fee set", async () => {
         const feeBalance = amount;
-        // mock lrc to return 100 either
+        // mock lrc to return feeBalance
         const getBalanceOfFee = web3.utils
           .sha3("balanceOf(address)")
           .slice(0, 10);
-
         await mockLRC.givenMethodReturn(
           getBalanceOfFee,
           abi.rawEncode(["uint"], [feeBalance])
@@ -139,11 +125,8 @@ contract("ProtocolFeeVault", (accounts: string[]) => {
 
         // mock lrc burn return true
         const lrcBurn = web3.utils.sha3("burn(uint256)").slice(0, 10);
+        await mockLRC.givenMethodReturnBool(lrcBurn, true);
 
-        await mockLRC.givenMethodReturnBool(
-          lrcBurn,
-          abi.rawEncode(["bool"], [true])
-        );
         const tx = await protocolFeeVault.fundDAO();
         truffleAssert.eventEmitted(tx, "DAOFunded", (evt: any) => {
           return (
@@ -160,6 +143,93 @@ contract("ProtocolFeeVault", (accounts: string[]) => {
               ))
           );
         });
+      });
+
+      it("fundDao when fee add another amount", async () => {
+        const feeBalance = amount
+          .mul(new BN(REWARD_PERCENTAGE))
+          .div(new BN(100))
+          .add(amount2);
+        // mock lrc to return feeBalance
+        const getBalanceOfFee = web3.utils
+          .sha3("balanceOf(address)")
+          .slice(0, 10);
+        await mockLRC.givenMethodReturn(
+          getBalanceOfFee,
+          abi.rawEncode(["uint"], [feeBalance])
+        );
+
+        const tx = await protocolFeeVault.fundDAO();
+        truffleAssert.eventEmitted(tx, "DAOFunded", (evt: any) => {
+          return (
+            (evt.amountDAO == 0 && evt.amountBurn == 0) ||
+            (isAmountCloseEnough(
+              evt.amountDAO,
+              amount2.mul(new BN(DAO_PERDENTAGE)).div(new BN(100))
+            ) &&
+              isAmountCloseEnough(
+                evt.amountBurn,
+                amount2
+                  .mul(new BN(100 - REWARD_PERCENTAGE - DAO_PERDENTAGE))
+                  .div(new BN(100))
+              ))
+          );
+        });
+      });
+
+      it("getProtocolFeeStats check stats", async () => {
+        const feeBalance = amount
+          .add(amount2)
+          .mul(new BN(REWARD_PERCENTAGE))
+          .div(new BN(100));
+        // mock lrc to return feeBalance
+        const getBalanceOfFee = web3.utils
+          .sha3("balanceOf(address)")
+          .slice(0, 10);
+        await mockLRC.givenMethodReturn(
+          getBalanceOfFee,
+          abi.rawEncode(["uint"], [feeBalance])
+        );
+        const {
+          0: accumulatedFees,
+          1: accumulatedBurn,
+          2: accumulatedDAOFund,
+          3: accumulatedReward,
+          4: remainingFees,
+          5: remainingBurn,
+          6: remainingDAOFund,
+          7: remainingReward
+        } = await protocolFeeVault.getProtocolFeeStats();
+
+        assert(
+          isAmountCloseEnough(accumulatedFees, amount.add(amount2)) &&
+            isAmountCloseEnough(
+              accumulatedBurn,
+              amount
+                .add(amount2)
+                .mul(new BN(100 - REWARD_PERCENTAGE - DAO_PERDENTAGE))
+                .div(new BN(100))
+            ) &&
+            isAmountCloseEnough(
+              accumulatedDAOFund,
+              amount
+                .add(amount2)
+                .mul(new BN(DAO_PERDENTAGE))
+                .div(new BN(100))
+            ) &&
+            isAmountCloseEnough(
+              accumulatedReward,
+              amount
+                .add(amount2)
+                .mul(new BN(REWARD_PERCENTAGE))
+                .div(new BN(100))
+            ) &&
+            isAmountCloseEnough(remainingFees, feeBalance) &&
+            (remainingBurn == 0 || isAmountCloseEnough(ZERO, remainingBurn)) &&
+            (remainingDAOFund == 0 || remainingDAOFund(ZERO, remainingBurn)) &&
+            isAmountCloseEnough(remainingReward, feeBalance),
+          "accumulatedFees"
+        );
       });
     });
 
@@ -195,43 +265,24 @@ contract("ProtocolFeeVault", (accounts: string[]) => {
 
         // mock tokenSeller sellToken return true
         const sellToken = web3.utils
-          .sha3("sellToken(address, address)")
+          .sha3("sellToken(addres, address)")
           .slice(0, 10);
+        await mockTokenSeller.givenMethodReturnBool(sellToken, true);
 
-        await mockTokenSeller.givenMethodReturnBool(
-          sellToken,
-          abi.rawEncode(["bool"], [true])
-        );
-        await protocolFeeVault.sellTokenForLRC(mockToken.address, amount);
+        // await protocolFeeVault.sellTokenForLRC(mockToken.address, amount);
       });
     });
 
-    describe("getProtocolFeeStats", () => {
-      it("getProtocolFeeStats check result value", async () => {
-        //         const getBalanceOfFee = web3.utils
-        //           .sha3("balanceOf(address)")
-        //           .slice(0, 10);
-
-        //         await mockLRC.givenMethodReturn(
-        //           getBalanceOfFee,
-        //           abi.rawEncode(
-        //             ["uint"],
-        //             [ZERO]
-        //           )
-        //         );
-
-        const {
-          0: accumulatedFees,
-          1: accumulatedBurn,
-          2: accumulatedDAOFund,
-          3: accumulatedReward,
-          4: remainingFees,
-          5: remainingBurn,
-          6: remainingDAOFund,
-          7: remainingReward
-        } = await protocolFeeVault.getProtocolFeeStats();
-
-        assert(isAmountCloseEnough(accumulatedFees, amount), "accumulatedFees");
+    describe("claimStakingReward", () => {
+      it("claimStakingReward need called by userStakingPoolAddress", async () => {
+        await expectThrow(protocolFeeVault.claimStakingReward(0), "ZERO_VALUE");
+        await expectThrow(
+          protocolFeeVault.claimStakingReward(amount),
+          "UNAUTHORIZED"
+        );
+        await protocolFeeVault.claimStakingReward(amount, {
+          from: userStakingPoolAddress
+        });
       });
     });
   });
