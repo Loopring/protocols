4adcff92d7e66e3b896154051f1a31258f54c277
Daniel Wang <daniel@loopring.org>
2019-10-18 13:26:54 -0400

    [Protocol3] improve SignatureBasedAddressWhitelist

--- packages/loopring_v3/contracts/impl/SignatureBasedAddressWhitelist.sol
+++ packages/loopring_v3/contracts/impl/SignatureBasedAddressWhitelist.sol
@@ -20,6 +20,8 @@ import "../lib/Claimable.sol";
 
 import "../iface/IAddressWhitelist.sol";
 
+import "../thirdparty/ECDSA.sol";
+
 
 /// @title An Implementation of IAddressWhitelist.
 /// @author Daniel Wang  - <daniel@loopring.org>
@@ -36,38 +38,24 @@ contract SignatureBasedAddressWhitelist is Claimable, IAddressWhitelist
         public
         returns (bool)
     {
-        uint    t;
-        bytes32 r;
-        bytes32 s;
-        uint8   v;
-
         if (permission.length != 73) {
             return false;
         }
 
+        uint   time;
+        bytes  memory sig;
+
         assembly {
-            t := and(mload(add(permission, 8)), 0xFFFFFFFFFFFFFFFF) // first 8 bytes as time in second since epoch
-            r := mload(add(permission, 40))
-            s := mload(add(permission, 72))
-            v := and(mload(add(permission, 73)), 255)
+            time := and(mload(add(permission, 8)), 0xFFFFFFFFFFFFFFFF) // first 8 bytes as time in second since epoch
+            sig := mload(add(permission, 73))
         }
 
-        if (t < now - PERMISSION_TIMEOUT) {
+        if (time < now - PERMISSION_TIMEOUT) {
             return false;
         }
 
-        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
-            return false;
-        }
-
-        if (v < 27) {
-            v += 27;
-        }
-        if (v != 27 && v != 28) {
-            return false;
-        }
-        bytes32 msgBase = keccak256(abi.encodePacked("LOOPRING_DEX_ACCOUNT_CREATION", addr, t));
-        bytes32 hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", msgBase));
-        return owner == ecrecover(hash, v, r, s);
+        bytes32 msgBase = keccak256(abi.encodePacked("LOOPRING_DEX_ACCOUNT_CREATION", addr, time));
+        bytes32 hash = ECDSA.toEthSignedMessageHash(msgBase);
+        return owner == ECDSA.recover(hash, sig);
     }
 }
