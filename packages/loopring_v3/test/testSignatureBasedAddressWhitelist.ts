98fe18417210063732c90d062855d8fd5efa70d2
Daniel Wang <daniel@loopring.org>
2019-10-18 14:47:48 -0400

    [Protocol3] improve SignatureBasedAddressWhitelist

--- packages/loopring_v3/test/testSignatureBasedAddressWhitelist.ts
+++ packages/loopring_v3/test/testSignatureBasedAddressWhitelist.ts
@@ -1,15 +1,10 @@
 import { Bitstream } from "loopringV3.js";
 import { ExchangeTestUtil } from "./testExchangeUtil";
-import BN = require("bn.js");
-import Contract from "web3/eth/contract";
-const fs = require("fs");
 const abi = require("ethereumjs-abi");
 
 contract("Exchange", (accounts: string[]) => {
   let exchangeTestUtil: ExchangeTestUtil;
   let newAddressWhitelist: any;
-  let addressWhiteListABI: any;
-  let addressWhiteListContract: Contract;
 
   before(async () => {
     exchangeTestUtil = new ExchangeTestUtil();
@@ -19,16 +14,6 @@ contract("Exchange", (accounts: string[]) => {
       newAddressWhitelist.address != 0,
       "newAddressWhitelist.address == 0."
     );
-
-    const ABIPath = "ABI/version30/";
-    addressWhiteListABI = fs.readFileSync(
-      ABIPath + "IAddressWhitelist.abi",
-      "ascii"
-    );
-    addressWhiteListContract = new web3.eth.Contract(
-      JSON.parse(addressWhiteListABI),
-      newAddressWhitelist.address
-    );
   });
 
   const generatePermissionBytes = async (
@@ -37,6 +22,7 @@ contract("Exchange", (accounts: string[]) => {
     signer: any
   ) => {
     const bitstream = new Bitstream();
+
     const hashMsg =
       "0x" +
       abi
@@ -45,7 +31,6 @@ contract("Exchange", (accounts: string[]) => {
           ["LOOPRING_DEX_ACCOUNT_CREATION", address, now]
         )
         .toString("hex");
-    // console.log('hashMsg :', hashMsg);
     const rsv = await web3.eth.sign(hashMsg, signer);
     bitstream.addHex(rsv);
     bitstream.addNumber(now, 8);
@@ -54,7 +39,7 @@ contract("Exchange", (accounts: string[]) => {
     const permission = web3.utils.hexToBytes(bitstream.getData());
     assert(
       permission.length == 73,
-      "permission.length should be 73(sign65+t8)"
+      "permission.length should be 73(t8+sign65)"
     );
     return permission;
   };
@@ -71,19 +56,17 @@ contract("Exchange", (accounts: string[]) => {
         deployer
       );
 
-      addressWhiteListContract.methods
-        .isAddressWhitelisted(realAccount, permission)
-        .call()
-        .then((ret: any) => {
-          assert(ret, "Corrent whitelist signature should pass");
-        });
+      var ret = await newAddressWhitelist.isAddressWhitelisted(
+        realAccount,
+        permission
+      );
+      assert(ret, "isAddressWhitelisted(realOwner, permission) failed.");
 
-      addressWhiteListContract.methods
-        .isAddressWhitelisted(fakeAccount, permission)
-        .call()
-        .then((ret: any) => {
-          assert(!ret, "Corrent whitelist signature should not pass");
-        });
+      ret = await newAddressWhitelist.isAddressWhitelisted(
+        fakeAccount,
+        permission
+      );
+      assert(!ret, "fakeAccount's request should not pass.");
     });
 
     it("check isAddressWhitelisted fail conditions", async () => {
@@ -98,74 +81,21 @@ contract("Exchange", (accounts: string[]) => {
         deployer
       );
 
-      addressWhiteListContract.methods
-        .isAddressWhitelisted(realAccount, [])
-        .call()
-        .then((ret: any) => {
-          assert(!ret, "Wrong permission should not pass check.");
-        });
+      var ret = await newAddressWhitelist.isAddressWhitelisted(realAccount, []);
+      assert(!ret, "Wrong permission should not pass check.");
 
-      addressWhiteListContract.methods
-        .isAddressWhitelisted(realAccount, permission)
-        .call()
-        .then((ret: any) => {
-          assert(!ret, "Requests happened 1 day ago should not pass.");
-        });
-
-      var now = Math.floor(Date.now() / 1000);
-      var permission = await generatePermissionBytes(
-        now,
+      ret = await newAddressWhitelist.isAddressWhitelisted(
         realAccount,
-        deployer
+        permission
       );
-      permission[72] = 2;
+      assert(!ret, "Requests happened 1 day ago should not pass.");
 
-      addressWhiteListContract.methods
-        .isAddressWhitelisted(realAccount, permission)
-        .call()
-        .then((ret: any) => {
-          assert(!ret, "v is not in [0, 1] should not pass.");
-        });
-    });
-
-    it("check malicious permission", async () => {
-      const deployer = exchangeTestUtil.testContext.deployer;
-      const realAccount = exchangeTestUtil.testContext.orderOwners[0];
-      var now = Math.floor(Date.now() / 1000);
-      var permission = await generatePermissionBytes(
-        now,
+      permission[7] = 2;
+      ret = await newAddressWhitelist.isAddressWhitelisted(
         realAccount,
-        deployer
+        permission
       );
-
-      var permStr = await web3.utils.bytesToHex(permission);
-      var t = permStr.slice(2, 18);
-      var r = permStr.slice(18, 82);
-      var s = permStr.slice(82, 146);
-      var v = permStr.slice(146);
-
-      var curveN = new BN(
-        "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141",
-        16
-      );
-      var maliciousS = curveN.sub(new BN(s, 16));
-      var maliciousV = new BN(v, 16).add(new BN(1)).mod(new BN(2));
-
-      const bitstream = new Bitstream();
-      bitstream.addBN(new BN(t, 16), 8);
-      bitstream.addBN(new BN(r, 16), 32);
-      bitstream.addBN(maliciousS, 32);
-      bitstream.addBN(maliciousV, 1);
-
-      var maliciousPermission = bitstream.getData();
-
-      var maliciousPermBytes = await web3.utils.hexToBytes(maliciousPermission);
-      addressWhiteListContract.methods
-        .isAddressWhitelisted(realAccount, maliciousPermBytes)
-        .call()
-        .then((ret: any) => {
-          assert(!ret, "malicious permission should not pass");
-        });
+      assert(!ret, "v is not in [0, 1] should not pass.");
     });
   });
 });
