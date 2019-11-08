074e3e15feb6ae412cc2a33e7afeeb369c66f4c9
Daniel Wang <daniel@loopring.org>
2019-11-08 10:47:59 -0500

    add LRCBalanceBasedAddressWhitelist

---
+++ packages/loopring_v3/contracts/impl/LRCBalanceBasedAddressWhitelist.sol
@@ -0,0 +1,95 @@
+/*
+
+  Copyright 2017 Loopring Project Ltd (Loopring Foundation).
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+pragma solidity ^0.5.11;
+
+import "../lib/Claimable.sol";
+import "../lib/ERC20.sol";
+import "../lib/MathUint.sol";
+
+import "../iface/IAddressWhitelist.sol";
+
+
+/// @title An Implementation of IAddressWhitelist.
+/// @author Daniel Wang  - <daniel@loopring.org>
+
+// See https://etherscan.io/address/0xc8fcc48d1454a83589169294470549a2e1713dec#code
+contract LTIP {
+    struct Record {
+        uint lrcAmount;
+        uint timestamp;
+    }
+
+    mapping (address => Record) public records;
+}
+
+contract LRCBalanceBasedAddressWhitelist is Claimable, IAddressWhitelist
+{
+    using MathUint for uint;
+    address public constant LTIPAddr = 0xC8Fcc48D1454a83589169294470549A2e1713DeC;
+    address public constant LRCToken = 0xBBbbCA6A901c926F240b89EacB641d8Aec7AEafD;
+
+    uint public minLRCRequired = 0;
+
+    constructor() Claimable() public {}
+
+    event MinLRCRequiredUpdated(uint value);
+
+    function setMinLRCRequired(
+        uint _minLRCRequired
+        )
+        external
+        onlyOwner
+    {
+        minLRCRequired = _minLRCRequired;
+        emit MinLRCRequiredUpdated(minLRCRequired);
+    }
+
+    function isAddressWhitelisted(
+        address addr,
+        bytes   memory // not used
+        )
+        public
+        view
+        returns (bool)
+    {
+        return hasEnoughLRC(addr);
+    }
+
+    function hasEnoughLRC(address addr)
+        public
+        view
+        returns (bool)
+    {
+        if (minLRCRequired == 0) {
+            return true;
+        }
+
+        uint accountBalance = ERC20(LRCToken).balanceOf(addr);
+        if (accountBalance >= minLRCRequired) {
+            return true;
+        }
+
+        (uint ltipBalance,) = LTIP(LTIPAddr).records(addr);
+
+        if (accountBalance.add(ltipBalance) >= minLRCRequired) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
