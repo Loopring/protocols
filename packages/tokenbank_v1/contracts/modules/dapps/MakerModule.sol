d496e9a753bf8e8938393f86cb9e711dbbbbeeae
Daniel Wang <daniel@loopring.org>
2019-11-22 11:05:17 -0500

    minor change

---
+++ packages/tokenbank_v1/contracts/modules/dapps/MakerModule.sol
@@ -0,0 +1,42 @@
+/*
+
+  Copyright 2017 Loopring Project Ltd (Loopring Foundation).
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+pragma solidity ^0.5.11;
+
+import "../../lib/AddressUtil.sol";
+import "../../lib/MathUint.sol";
+import "../../lib/SignatureUtil.sol";
+
+import "../../thirdparty/ERC1271.sol";
+
+import "../../iface/Wallet.sol";
+
+import "../../base/MetaTxModule.sol";
+
+import "../storage/AccessGuardianStorage.sol";
+import "../storage/GuardianStorage.sol";
+
+
+/// @title MakerModule
+contract MakerModule is MetaTxModule, AccessGuardianStorage
+{
+
+    constructor(GuardianStorage _guardianStorage)
+        public
+        AccessGuardianStorage(_guardianStorage)
+    {
+    }
+}
