92f16af1ef54584f39bb856ede61ad851f7ed476
Daniel Wang <daniel@loopring.org>
2019-11-24 13:26:49 -0500

    implement TransferModule

--- packages/tokenbank_v1/contracts/modules/transfers/TransferModule.sol
+++ packages/tokenbank_v1/contracts/modules/transfers/TransferModule.sol
@@ -24,5 +24,83 @@ import "../../base/BaseModule.sol";
 /// @title TransferModule
 contract TransferModule is BaseModule
 {
+        // Empty calldata
+    bytes constant internal EMPTY_BYTES = "";
+    // Mock token address for ETH
+    address constant internal ETH_TOKEN = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
+    // *************** Events *************************** //
+
+    event Transfer(
+        address indexed wallet,
+        address indexed token,
+        address indexed to,
+        uint            amount,
+        bytes           data
+    );
+    event Approval(
+        address indexed wallet,
+        address indexed token,
+        address         spender,
+        uint            amount
+    );
+    event Transacted(
+        address indexed wallet,
+        address indexed to,
+        uint            amount,
+        bytes           data
+    );
+
+    function transferInternal(
+        address wallet,
+        address token,
+        address to,
+        uint    amount,
+        bytes   memory data
+        )
+        internal
+    {
+        if (token == address(0)) {
+            transact(wallet, to, amount, "");
+        } else {
+            bytes memory callData = abi.encodeWithSignature(
+                "transfer(address,uint)",
+                to,
+                amount
+            );
+            transact(wallet, token, 0, callData);
+        }
+        emit Transfer(wallet, token, to, amount, data);
+    }
+
+    function approveInternal(
+        address wallet,
+        address token,
+        address spender,
+        uint    amount
+        )
+        internal
+    {
+        require(token != address(0), "UNSUPPORTED");
+
+        bytes memory callData = abi.encodeWithSignature(
+            "approve(address,uint)",
+            spender,
+            amount
+        );
+        transact(wallet, token, 0, callData);
+        emit Approval(wallet, token, spender, amount);
+    }
+
+    function transactInternal(
+        address wallet,
+        address to,
+        uint    amount,
+        bytes   memory data
+        )
+        internal
+    {
+        transact(wallet, to, amount, data);
+        emit Transacted(wallet, to, amount, data);
+    }
 }
