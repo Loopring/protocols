4cd53ca96b1354ab41eca2cb41967324096aba13
yueawang <wwymoving@gmail.com>
2019-11-27 02:51:33 +0800

    [tokenbank] Price provider implementation (#757)

--- packages/tokenbank_v1/contracts/price/UniswapPriceOracle.sol
+++ packages/tokenbank_v1/contracts/price/UniswapPriceOracle.sol
@@ -18,11 +18,45 @@ pragma solidity ^0.5.11;
 
 import "../iface/PriceOracle.sol";
 
+contract UniswapFactoryInterface {
+    // Get Exchange and Token Info
+    function getExchange(address token)    external view returns (address exchange);
+    function getToken   (address exchange) external view returns (address token);
+}
+
+contract UniswapExchangeInterface {
+    // Address of ERC20 token sold on this exchange
+    function tokenAddress() external view returns (address token);
+    // Get Prices
+    function getEthToTokenInputPrice (uint eth_sold)      external view returns (uint tokens_bought);
+    function getEthToTokenOutputPrice(uint tokens_bought) external view returns (uint eth_sold);
+    function getTokenToEthInputPrice (uint tokens_sold)   external view returns (uint eth_bought);
+    function getTokenToEthOutputPrice(uint eth_bought)    external view returns (uint tokens_sold);
+}
+
 /// @title UniswapPriceOracle
+/// @dev Return the value in Ether for any given ERC20 token.
 contract UniswapPriceOracle is PriceOracle
 {
+    UniswapFactoryInterface uniswapFactory;
+
+    constructor(UniswapFactoryInterface _uniswapFactory)
+        public
+    {
+        uniswapFactory = _uniswapFactory;
+    }
+
     function tokenPrice(address token, uint amount)
         public
         view
-        returns (uint value);
+        returns (uint value)
+    {
+        if (amount == 0) return 0;
+        if (token == address(0)) return amount;
+
+        address exchange = uniswapFactory.getExchange(token);
+        if (exchange == address(0)) return 0; // no exchange
+
+        return UniswapExchangeInterface(exchange).getTokenToEthInputPrice(amount);
+    }
 }
