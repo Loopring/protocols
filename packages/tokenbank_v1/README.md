4c3f945acd79de91492ed1455ad4692fcf8d0a7c
Daniel Wang <daniel@loopring.org>
2019-11-27 11:12:31 -0500

    rename tokenbank => hebao

--- packages/tokenbank_v1/README.md
+++
@@ -1,2 +0,0 @@
-# TokenBank Smart Wallet Contracts
-
--- packages/tokenbank_v1/util/Artifacts.ts
+++
@@ -1,7 +0,0 @@
-export class Artifacts {
-  public MockContract: any;
-
-  constructor(artifacts: any) {
-    this.MockContract = artifacts.require("thirdparty/MockContract.sol");
-  }
-}
