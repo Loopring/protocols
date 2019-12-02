b2cf0c25d57d357b36d4205f7b8e6f58ee2efb0e
Brecht Devos <Brechtp.Devos@gmail.com>
2019-12-03 00:45:24 +0100

    [hebao] Work around Istanbul gas cost changes for send/transfer (#807)
    * [hebao] Work around Istanbul gas cost changes
    
    * [hebao] Removed the Received event

--- packages/hebao_v1/contracts/base/BaseWallet.sol
+++ packages/hebao_v1/contracts/base/BaseWallet.sol
@@ -56,12 +56,6 @@ contract BaseWallet is Wallet, AddressSet, ReentrancyGuard
         bytes           data
     );
 
-    event Received(
-        address indexed sender,
-        uint    value,
-        bytes   data
-    );
-
     modifier onlyOwner
     {
         require(msg.sender == _owner, "NOT_A_OWNER");
@@ -223,12 +217,8 @@ contract BaseWallet is Wallet, AddressSet, ReentrancyGuard
         external
         payable
     {
-        if (msg.value > 0) {
-            emit Received(msg.sender, msg.value, msg.data);
-            return;
-        }
-
-        if (msg.data.length == 0) {
+        if (msg.data.length == 0 || msg.value > 0) {
+            // Note: don't do anything here so send/transfer still works (because of the 2300 gas limit)
             return;
         }
 
