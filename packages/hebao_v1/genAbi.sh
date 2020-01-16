e5de062a243d1d71d7ece448be450499ba773906
kongliangzhong <kongliangzhong@gmail.com>
2020-01-16 16:05:14 +0800

    [HEABO] update genAbi.sh

--- packages/hebao_v1/genAbi.sh
+++ packages/hebao_v1/genAbi.sh
@@ -6,15 +6,6 @@ mkdir $ABI_PATH
 
 solc \
     -o ABI/ --overwrite \
-    --abi contracts/thirdparty/*.sol
-
-solc \
-    -o ABI/ --overwrite \
-    --abi contracts/lib/*.sol \
-    --allow-paths contracts/thirdparty/*.sol
-
-solc \
-    -o ABI/ --overwrite \
     --abi contracts/iface/*.sol \
     --allow-paths contracts/thirdparty/*.sol contracts/lib/*.sol \
     contracts/base/*.sol contracts/stores/*.sol
@@ -24,6 +15,15 @@ solc \
     --abi contracts/base/*.sol \
     --allow-paths contracts/thirdparty/*.sol contracts/lib/*.sol \
     contracts/iface/*.sol contracts/stores/*.sol
+
+solc \
+    -o ABI/ --overwrite \
+    --abi contracts/modules/core/*.sol \
+    contracts/modules/dapps/*.sol contracts/modules/exchanges/*.sol \
+    contracts/modules/transfers/*.sol contracts/modules/security/*.sol \
+    --allow-paths contracts/thirdparty/*.sol contracts/lib/*.sol \
+    contracts/iface/*.sol contracts/stores/*.sol \
+    contracts/base/*.sol contracts/modules/security/*.sol \
 
 # for file in $ABI_PATH/*
 # do
