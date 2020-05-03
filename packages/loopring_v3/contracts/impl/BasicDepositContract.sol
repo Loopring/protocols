5c78c73d0e7dc189ee5695c6cc4f07676c7b8dd3
wangdong <wangdong@zhongan.io>
2020-05-03 15:36:21 +0800

    minor improve BasicDepositContract

--- packages/loopring_v3/contracts/impl/BasicDepositContract.sol
+++ packages/loopring_v3/contracts/impl/BasicDepositContract.sol
@@ -199,14 +199,12 @@ contract BasicDepositContract is IDepositContract, ReentrancyGuard
         )
         internal
     {
-        if (amount > 0) {
-            if (token == address(0)) {
-                // ETH
-                to.sendETHAndVerify(amount, gasleft());
-            } else {
-                // ERC20 token
-                token.safeTransferAndVerify(to, amount);
-            }
+        if (amount == 0) return;
+        
+        if (isETHInternal(token)) {
+            to.sendETHAndVerify(amount, gasleft());
+        } else {
+            token.safeTransferAndVerify(to, amount);
         }
     }
 }
