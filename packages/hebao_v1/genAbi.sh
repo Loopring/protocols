21a7923eb652526038a9810457d50cc3cfd22698
kongliangzhong <kongliangzhong@gmail.com>
2020-05-06 12:10:01 +0800

    update solc,truffle version and genAbi.sh

--- packages/hebao_v1/genAbi.sh
+++ packages/hebao_v1/genAbi.sh
@@ -4,9 +4,31 @@ ABI_PATH="./ABI"
 rm -rf $ABI_PATH
 mkdir $ABI_PATH
 
+if ! type "solc" > /dev/null; then
+    case "$OSTYPE" in
+        darwin*)
+            echo "OS: MacOS"
+            brew install ethereum/ethereum/solidity
+            ;;
+
+        linux*)
+            echo "OS: Linux"
+            sudo add-apt-repository ppa:ethereum/ethereum
+            sudo apt-get update
+            sudo apt-get install solc
+            ;;
+
+        *)
+            echo "unsupported OS: $OSTYPE"
+            exit 0
+            ;;
+    esac
+fi
+
+
 solc \
     -o ABI/ --overwrite \
-    --abi contracts/iface/*.sol \
+    --abi contracts/iface/*.sol
     --allow-paths contracts/thirdparty/*.sol contracts/lib/*.sol \
     contracts/base/*.sol contracts/stores/*.sol
 
