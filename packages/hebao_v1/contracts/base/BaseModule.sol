07a0e7a201de79839b2e754a6266d87e2d25eb7d
Daniel Wang <daniel@loopring.org>
2020-06-12 10:54:38 +0800

    improve gas usage

--- packages/hebao_v1/contracts/base/BaseModule.sol
+++ packages/hebao_v1/contracts/base/BaseModule.sol
@@ -116,8 +116,8 @@ contract BaseModule is ReentrancyGuard, Module
     ///         when this module is activated for the wallet.
     function bindableMethods()
         public
-        virtual
         pure
+        virtual
         returns (bytes4[] memory methods)
     {
         return new bytes4[](0);
