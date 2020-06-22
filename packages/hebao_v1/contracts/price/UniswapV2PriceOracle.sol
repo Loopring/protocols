2c3cac7674375b90619caacc07493d989bfaca1b
kongliangzhong <kongliangzhong@gmail.com>
2020-06-22 17:11:53 +0800

    [hebao_v1] fix UniswapV2PriceOracle

--- packages/hebao_v1/contracts/price/UniswapV2PriceOracle.sol
+++ packages/hebao_v1/contracts/price/UniswapV2PriceOracle.sol
@@ -18,6 +18,8 @@ pragma solidity ^0.6.6;
 
 import "../thirdparty/uniswap2/IUniswapV2Factory.sol";
 import "../thirdparty/uniswap2/IUniswapV2Pair.sol";
+import "../thirdparty/uniswap2/FixedPoint.sol";
+import "../thirdparty/uniswap2/UniswapV2OracleLibrary.sol";
 
 import "../iface/PriceOracle.sol";
 
@@ -27,6 +29,7 @@ import "../lib/MathUint.sol";
 /// @dev Returns the value in Ether for any given ERC20 token.
 contract UniswapV2PriceOracle is PriceOracle
 {
+    using FixedPoint for *;
     using MathUint for uint;
 
     IUniswapV2Factory uniswapV2Factory;
@@ -42,6 +45,20 @@ contract UniswapV2PriceOracle is PriceOracle
         wethAddress = _wethAddress;
     }
 
+    // given the cumulative prices of the start and end of a period,
+    // and the length of the period, compute the average
+    // price in terms of how much amount out is received for the amount in
+    function computeAmountOut(
+        uint priceCumulativeStart, uint priceCumulativeEnd,
+        uint timeElapsed, uint amountIn
+    ) private pure returns (uint amountOut) {
+        // overflow is desired.
+        FixedPoint.uq112x112 memory priceAverage = FixedPoint.uq112x112(
+            uint224((priceCumulativeEnd - priceCumulativeStart) / timeElapsed)
+        );
+        amountOut = priceAverage.mul(amountIn).decode144();
+    }
+
     // Using UniswapV2's sliding window price.
     function tokenValue(address token, uint amount)
         public
@@ -55,6 +72,35 @@ contract UniswapV2PriceOracle is PriceOracle
         address pair = uniswapV2Factory.getPair(token, wethAddress);
         if (pair == address(0)) return 0; // no pair
 
-        return IUniswapV2Pair(pair).price1CumulativeLast().mul(amount);
+        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) =
+            IUniswapV2Pair(pair).getReserves();
+
+        uint timeElapsed = block.timestamp - blockTimestampLast;
+        (uint price0Cumulative, uint price1Cumulative,) =
+            UniswapV2OracleLibrary.currentCumulativePrices(pair);
+        (address token0,) = sortTokens(token, wethAddress);
+        if (token0 == token) {
+            return computeAmountOut(reserve0,
+                                    price0Cumulative,
+                                    timeElapsed,
+                                    amount);
+        } else {
+            return computeAmountOut(reserve1,
+                                    price1Cumulative,
+                                    timeElapsed,
+                                    amount);
+        }
+    }
+
+    /// returns sorted token addresses, used to handle return values from pairs sorted in this order
+    /// taken from UniswapV2Library
+    function sortTokens(address tokenA, address tokenB)
+        internal
+        pure
+        returns (address token0, address token1)
+    {
+        require(tokenA != tokenB, "UniswapV2Library: IDENTICAL_ADDRESSES");
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), "UniswapV2Library: ZERO_ADDRESS");
     }
 }
