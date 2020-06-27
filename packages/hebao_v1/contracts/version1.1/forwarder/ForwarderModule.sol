7087111e11ba5d13affd7cf7c76cae4201487aa7
wangdong <wangdong@zhongan.io>
2020-06-27 21:24:36 +0800

    more

--- packages/hebao_v1/contracts/version1.1/forwarder/ForwarderModule.sol
+++ packages/hebao_v1/contracts/version1.1/forwarder/ForwarderModule.sol
@@ -17,43 +17,135 @@
 pragma solidity ^0.6.6;
 pragma experimental ABIEncoderV2;
 
-import "../../lib/MathUint.sol";
+import "../../lib/EIP712.sol";
+import "../../lib/SignatureUtil.sol";
 import "../base/BaseModule.sol";
-import "./Forwarder.sol";
 
 
 /// @title ForwarderModule
 /// @dev Base contract for all smart wallet modules.
 ///
 /// @author Daniel Wang - <daniel@loopring.org>
-abstract contract ForwarderModule is BaseModule, Forwarder
+contract ForwarderModule is BaseModule
 {
-    using MathUint for uint;
-    uint public constant GAS_OVERHEAD = 200000; // TODO
+    using SignatureUtil for bytes32;
+
+    uint    public constant GAS_OVERHEAD = 200000; // TODO
+    bytes32 public DOMAIN_SEPARATOR;
 
     constructor(ControllerImpl _controller)
         public
-        BaseModule(_controller) {}
-
-    function beforeExecute(MetaTx memory metaTx)
-        internal
-        override
-        returns (bool /*abort*/)
+        BaseModule(_controller)
     {
-        require(gasleft() >= (metaTx.gasLimit.mul(64) / 63).add(GAS_OVERHEAD), "INSUFFICIENT_GAS");
-        // require(controller.walletRegistry().isWalletRegistered(metaTx.from), "NOT_A_WALLET");
+        DOMAIN_SEPARATOR = EIP712.hash(
+            EIP712.Domain("Loopring Wallet MetaTx", "2.0", address(0))
+        );
     }
 
-    function afterExecute(
-        MetaTx memory metaTx,
-        bool          /*success*/,
-        bytes  memory /*returnValue*/,
-        uint          gasUsed
-        )
-        internal
-        override
-        returns(bool /*abort*/)
+    event MetaTxExecuted(
+        address indexed relayer,
+        address indexed from,
+        uint            nonce
+    );
+
+    struct MetaTx {
+        address from;
+        address to;
+        uint    nonce;
+        address gasToken;
+        uint    gasPrice;
+        uint    gasLimit;
+        bytes   data;
+    }
+
+    bytes32 constant public META_TX_TYPEHASH = keccak256(
+        "MetaTx(address from,address to,uint256 nonce,address gasToken,uint256 gasPrice,uint256 gasLimit,bytes data)"
+    );
+
+    mapping(address => uint256) public nonces;
+
+    function getNonce(address from)
+        external
+        view
+        returns(uint)
     {
+        return nonces[from];
+    }
+
+    function validateMetaTx(
+        address from,
+        address to,
+        uint    nonce,
+        address gasToken,
+        uint    gasPrice,
+        uint    gasLimit,
+        bytes   memory data,
+        bytes   memory signature
+        )
+        public
+        view
+    {
+        require(to != address(this), "CANNOT_FORWARD_TO_SELF");
+        require((nonce >> 128) <= (block.number), "NONCE_TOO_LARGE");
+        require(nonce > nonces[from], "NONCE_TOO_SMALL");
+
+        bytes memory encoded = abi.encodePacked(
+            META_TX_TYPEHASH,
+            from,
+            to,
+            nonce,
+            gasToken,
+            gasPrice,
+            gasLimit,
+            keccak256(data)
+        );
+
+        bytes32 metaTxHash = EIP712.hashPacked(DOMAIN_SEPARATOR, keccak256(encoded));
+        require(metaTxHash.verifySignature(from, signature), "INVALID_SIGNATURE");
+    }
+
+    function executeMetaTx(
+        MetaTx calldata metaTx,
+        bytes  calldata signature
+        )
+        external
+        nonReentrant
+        returns (
+            bool         success,
+            bytes memory returnValue
+        )
+    {
+        require(
+            gasleft() >= (metaTx.gasLimit.mul(64) / 63).add(GAS_OVERHEAD),
+            "INSUFFICIENT_GAS"
+        );
+
+        validateMetaTx(
+            metaTx.from,
+            metaTx.to,
+            metaTx.nonce,
+            metaTx. gasToken,
+            metaTx.gasPrice,
+            metaTx.gasLimit,
+            metaTx.data,
+            signature
+        );
+
+        uint gasLeft = gasleft();
+
+        nonces[metaTx.from] = metaTx.nonce;
+
+        (success, returnValue) = metaTx.to.call{gas : metaTx.gasLimit, value : 0}(
+            abi.encodePacked(metaTx.data, metaTx.from)
+        );
+
+        if (address(this).balance > 0) {
+            payable(controller.collectTo()).transfer(address(this).balance); 
+        }
+
+        emit MetaTxExecuted(msg.sender, metaTx.from, metaTx.nonce);
+
+        uint gasUsed = gasLeft - gasleft();
         uint gasAmount = gasUsed < metaTx.gasLimit ? gasUsed : metaTx.gasLimit;
 
         if (metaTx.gasPrice > 0) {
