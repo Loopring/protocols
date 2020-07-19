1f0b4070e4f43c61e97385137dce9725f28882f0
wangdong <wangdong@zhongan.io>
2020-07-19 20:57:00 +0800

    rename

--- packages/loopring_v3/contracts/impl/StatelessWallet.sol
+++ packages/loopring_v3/contracts/impl/StatelessWallet.sol
@@ -57,31 +57,7 @@ contract StatelessWallet
         )
     {
         require(wallet.accountID == accountID, "INVALID_WALLET_ACCOUNT");
-        {
-            bytes32[] memory guardianHashes = new bytes32[](wallet.guardians.length);
-            for (uint i = 0; i < wallet.guardians.length; i++) {
-                guardianHashes[i] = keccak256(
-                    abi.encode(
-                        GUARDIAN_TYPEHASH,
-                        wallet.guardians[i].addr,
-                        wallet.guardians[i].group
-                    )
-                );
-            }
-            bytes32 _walletDataHash = EIP712.hashPacked(
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encode(
-                        STATELESSWALLET_TYPEHASH,
-                        wallet.accountID,
-                        keccak256(abi.encodePacked(guardianHashes)),
-                        wallet.inheritor,
-                        wallet.inheritableSince
-                    )
-                )
-            );
-            require(_walletDataHash == walletDataHash, "INVALID_WALLET_DATA");
-        }
+        require(hashWallet(wallet) == walletDataHash, "INVALID_WALLET_DATA");
         _;
     }
 
@@ -145,6 +121,37 @@ contract StatelessWallet
         require(now >= wallet.inheritableSince, "TOO_SOON_TO_INHERIT");
         require(wallet.inheritor == newOwner, "WRONG_INHERITOR");
         return RECOVERY_MAGICVALUE;
+    }
+
+    function hashWallet(
+        Wallet memory wallet
+        )
+        public
+        view
+        returns (bytes32)
+    {
+       bytes32[] memory guardianHashes = new bytes32[](wallet.guardians.length);
+        for (uint i = 0; i < wallet.guardians.length; i++) {
+            guardianHashes[i] = keccak256(
+                abi.encode(
+                    GUARDIAN_TYPEHASH,
+                    wallet.guardians[i].addr,
+                    wallet.guardians[i].group
+                )
+            );
+        }
+        return EIP712.hashPacked(
+            DOMAIN_SEPARATOR,
+            keccak256(
+                abi.encode(
+                    STATELESSWALLET_TYPEHASH,
+                    wallet.accountID,
+                    keccak256(abi.encodePacked(guardianHashes)),
+                    wallet.inheritor,
+                    wallet.inheritableSince
+                )
+            )
+        );
     }
 
     // -- Internal --
