d1812244d507ec339885022a73748d8fdee93389
wangdong <wangdong@zhongan.io>
2020-07-19 21:04:33 +0800

    more

--- packages/loopring_v3/contracts/impl/libtransactions/TransferTransaction.sol
+++ packages/loopring_v3/contracts/impl/libtransactions/TransferTransaction.sol
@@ -44,12 +44,15 @@ library TransferTransaction
 
         // Check that this is a conditional transfer
         require(data.toUint8(offset) == 1, "INVALID_AUXILIARYDATA_DATA");
-        offset += 1;
+        // offset += 1;
 
         // Extract the transfer data
         //uint24 fromAccountID = data.toUint24(offset);
+        // offset += 3;
         //uint24 toAccountID = data.toUint24(offset);
-        offset += 6;
+        // offset += 3；
+
+        offset += 7;
         uint16 tokenID = data.toUint16(offset) >> 4;
         uint16 feeTokenID = uint16(data.toUint16(offset + 1) & 0xFFF);
         offset += 3;
