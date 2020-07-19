cffd27984aa33c94692ae3d33cb90a54a0a849da
wangdong <wangdong@zhongan.io>
2020-07-19 14:39:54 +0800

    aaa

--- packages/loopring_v3/contracts/impl/libtransactions/WithdrawTransaction.sol
+++ packages/loopring_v3/contracts/impl/libtransactions/WithdrawTransaction.sol
@@ -16,11 +16,13 @@ import "../libexchange/ExchangeWithdrawals.sol";
 
 /// @title WithdrawTransaction
 /// @author Brecht Devos - <brecht@loopring.org>
-/// @dev Four types of withdrawals are supported:
-///     withdrawalType = 0: offchain withdrawal with EdDSA signature
-///     withdrawalType = 1: offchian withdrawal with ECDSA signature or onchain withdrawl hash authorization.
-///     withdrawalType = 2: onchain forced full-amount withdrawal by and to the owner
-///     withdrawalType = 3: operator initiated full-amount withdrawal in shutdown mode.
+/// @dev The following 4 types of withdrawals are supported:
+///      - withdrawType = 0: offchain withdrawals with EdDSA signatures
+///      - withdrawType = 1: offchain withdrawals with ECDSA signatures or onchain appprovals
+///      - withdrawType = 2: onchain valid forced withdrawals (owner and accountID matche)
+///      - withdrawType = 3: onchain invalid forced withdrawals (owner and accountID mismatch)
+/// TODO(brecht):
+///      - withdrawType = 4: offchain operator-initiated withdrawals for protocol fees or in shutdown mode.
 library WithdrawTransaction
 {
     using BytesUtil            for bytes;
@@ -52,7 +54,7 @@ library WithdrawTransaction
     // Auxiliary data for each withdrawal
     struct WithdrawalAuxiliaryData
     {
-        uint  gasLimit;
+        uint gasLimit;
         bytes signature;
         bytes auxiliaryData;
     }
@@ -79,7 +81,7 @@ library WithdrawTransaction
         if (withdrawal.withdrawalType == 0) {
             // Signature checked offchain, nothing to do
         } else if (withdrawal.withdrawalType == 1) {
-            // Offchain withdrawal with ECDSA signature or hash authorization.
+            // Check appproval onchain
 
             // Calculate the tx hash
             bytes32 txHash = EIP712.hashPacked(
@@ -103,50 +105,53 @@ library WithdrawTransaction
 
             // Verify the signature if one is provided, otherwise fall back to an approved tx
             if (auxData.signature.length > 0) {
-                require(
-                    txHash.verifySignature(withdrawal.owner, auxData.signature),
-                    "INVALID_SIGNATURE"
-                );
+                require(txHash.verifySignature(withdrawal.owner, auxData.signature), "INVALID_SIGNATURE");
             } else {
                 require(S.approvedTx[withdrawal.owner][txHash], "TX_NOT_APPROVED");
-                delete S.approvedTx[withdrawal.owner][txHash];
+                S.approvedTx[withdrawal.owner][txHash] = false;
             }
         } else if (withdrawal.withdrawalType == 2 || withdrawal.withdrawalType == 3) {
             // Forced withdrawals cannot make use of certain features because the
             // necessary data is not authorized by the account owner.
+            // For protocol fee withdrawals, `owner` and `to` are both address(0).
             require(withdrawal.owner == withdrawal.to, "INVALID_WITHDRAWAL_ADDRESS");
+
+            // Forced withdrawal fees are charged when the request is submitted.
             require(withdrawal.fee == 0, "FEE_NOT_ZERO");
+
             require(auxData.auxiliaryData.length == 0, "AUXILIARY_DATA_NOT_ALLOWED");
 
             ExchangeData.ForcedWithdrawal storage forcedWithdrawal =
                 S.pendingForcedWithdrawals[withdrawal.accountID][withdrawal.tokenID];
 
-            if (forcedWithdrawal.timestamp == 0) {
-                // Allow the operator to submit full withdrawals without authorization
-                // - when in shutdown mode
-                // - to withdraw protocol fees
-                require(withdrawal.accountID == 0 || S.isShutdown(), "FULL_WITHDRAWAL_UNAUTHORIZED");
-            } else {
-                // Type == 2: valid onchain withdrawal started by the owner
-                // Type == 3: invalid onchain withdrawal started by someone else
-                bool authorized = (withdrawal.owner == forcedWithdrawal.owner);
-
-                require(authorized == (withdrawal.withdrawalType == 2), "INVALID_WITHDRAW_TYPE");
-                require(authorized || withdrawal.amount == 0, "UNAUTHORIZED_WITHDRAWAL");
+            if (forcedWithdrawal.timestamp != 0) {
+                if (withdrawal.withdrawalType == 2) {
+                    require(withdrawal.owner == forcedWithdrawal.owner, "INCONSISENT_OWNER");
+                } else { //withdrawal.withdrawalType == 3
+                    require(withdrawal.owner != forcedWithdrawal.owner, "INCONSISENT_OWNER");
+                    require(withdrawal.amount == 0, "UNAUTHORIZED_WITHDRAWAL");
+                }
 
                 // Get the fee
                 feeETH = forcedWithdrawal.fee;
 
-                // Reset the approval so it can't be used again
+                // delete the withdrawal request and free a slot
                 delete S.pendingForcedWithdrawals[withdrawal.accountID][withdrawal.tokenID];
-
-                // Open up a slot
                 S.numPendingForcedTransactions--;
 
                 emit ForcedWithdrawalProcessed(
                     withdrawal.accountID,
                     withdrawal.tokenID,
                     withdrawal.amount
+                );
+            } else {
+                // Allow the operator to submit fill withdrawals without authorization
+                // - when in shutdown mode
+                // - to withdraw protocol fees
+                require(
+                    withdrawal.owner == address(0) && withdrawal.accountID == 0 ||
+                    S.isShutdown(),
+                    "FULL_WITHDRAWAL_UNAUTHORIZED"
                 );
             }
         } else {
