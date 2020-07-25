f3f9e84e09d0f954bceaef4c56a6f7296dbbd2c8
wangdong <wangdong@zhongan.io>
2020-07-25 10:20:21 +0800

    add more code

--- packages/loopring_v3/contracts/impl/AgentRegistry.sol
+++ packages/loopring_v3/contracts/impl/AgentRegistry.sol
@@ -11,12 +11,90 @@ import "../lib/Claimable.sol";
 
 contract AgentRegistry is IAgentRegistry, AddressSet, Claimable
 {
-    function isAgent(address owner, address agent)
+    bytes32 internal constant UNIVERSAL_AGENTS = keccak256("__UNVERSAL_AGENTS__");
+
+	event AgentRegistered(
+		address user,
+		address agent,
+		bool    registered
+	);
+
+    constructor() public Claimable() {}
+
+    function isAgent(address user, address agent)
         external
         override
         view
         returns (bool)
     {
-        return false;
+        return isUnversalAgent(agent) || isUserAgent(user, agent);
+    }
+
+    function registerUniversalAgent(address agent, bool toRegister)
+    	external
+    	onlyOwner
+    {
+    	registerInternal(UNIVERSAL_AGENTS, agent, toRegister);
+    	emit AgentRegistered(address(0), agent, toRegister);
+    }
+
+    function getUniversalAgents()
+    	public
+    	view
+    	returns (address[] memory)
+    {
+    	return addressesInSet(UNIVERSAL_AGENTS);
+    }
+
+    function isUnversalAgent(address agent)
+    	public
+    	view
+    	returns (bool)
+    {
+    	return isAddressInSet(UNIVERSAL_AGENTS, agent);
+    }
+
+    function registerAgent(address agent, bool toRegister)
+    	external
+    {
+    	registerInternal(userKey(msg.sender), agent, toRegister);
+    	emit AgentRegistered(msg.sender, agent, toRegister);
+    }
+
+    function getAgents(address user)
+    	public
+    	view
+    	returns (address[] memory agents)
+    {
+    	if (user != address(0)) {
+	    	agents = addressesInSet(userKey(user));
+	    }
+    }
+
+	function isUserAgent(address user, address agent)
+    	public
+    	view
+    	returns (bool)
+    {
+    	return isAddressInSet(userKey(user), agent);
+    }
+
+	function registerInternal(bytes32 key, address agent, bool toRegister)
+		private
+	{
+		require(agent != address(0), "ZERO_ADDRESS");
+    	if (toRegister) {
+ 			addAddressToSet(key, agent, true);
+ 		} else {
+ 			removeAddressFromSet(key, agent);
+ 		}
+	}
+
+    function userKey(address addr)
+        private
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked("__AGENT__", addr));
     }
 }
