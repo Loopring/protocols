ec2374148a773d4bf711daae722115620b2ac4ea
kongliangzhong <kongliangzhong@gmail.com>
2020-08-02 21:48:21 +0800

    [hebao 1.1] improve isContact method

--- packages/hebao_v1/contracts/lib/AddressUtil.sol
+++ packages/hebao_v1/contracts/lib/AddressUtil.sol
@@ -17,9 +17,14 @@ library AddressUtil
         view
         returns (bool)
     {
-        uint32 size;
-        assembly { size := extcodesize(addr) }
-        return (size > 0);
+        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+        // for accounts without code, i.e. `keccak256('')`
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codehash := extcodehash(addr) }
+        return (codehash != accountHash && codehash != 0x0);
     }
 
     function toPayable(
