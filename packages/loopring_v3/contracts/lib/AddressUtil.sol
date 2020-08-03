c364e9ec830a80d048731a61dc4472b36707167e
Daniel Wang <daniel@loopring.org>
2020-08-03 13:35:13 +0800

    improve isContract

--- packages/loopring_v3/contracts/lib/AddressUtil.sol
+++ packages/loopring_v3/contracts/lib/AddressUtil.sol
@@ -16,9 +16,14 @@ library AddressUtil
         view
         returns (bool)
     {
-        uint32 size;
-        assembly { size := extcodesize(addr) }
-        return (size > 0);
+        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+        // for accounts without code, i.e. `keccak256('')`
+        bytes32 codehash;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codehash := extcodehash(addr) }
+        return (codehash != 0x0 &&
+                codehash != 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470);
     }
 
     function toPayable(
