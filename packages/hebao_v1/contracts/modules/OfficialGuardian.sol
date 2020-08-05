23cf978861ca0971fa1aefe5eb9ea1da7d0defe2
kongliangzhong <kongliangzhong@gmail.com>
2020-08-05 13:23:08 +0800

    [hebao 1.1] enable OfficialGuardian contract to transact (#1558)

--- packages/hebao_v1/contracts/modules/OfficialGuardian.sol
+++ packages/hebao_v1/contracts/modules/OfficialGuardian.sol
@@ -13,6 +13,7 @@ import "../thirdparty/BytesUtil.sol";
 contract OfficialGuardian is OwnerManagable, ERC1271
 {
     using SignatureUtil for bytes;
+    mapping (address => bool) public whitelist;
 
     function isValidSignature(
         bytes memory _data,
@@ -25,5 +26,31 @@ contract OfficialGuardian is OwnerManagable, ERC1271
     {
         (address addr1, address addr2) = _data.recoverECDSASigner(_signature);
         return isManager(addr1) || isManager(addr2) ?  ERC1271_MAGICVALUE : bytes4(0);
+    }
+
+    function addWhitelist(address target, bool toAdd)
+        external
+        onlyOwner
+    {
+        require(target != address(0), "ZERO_ADDRESS");
+        require(whitelist[target] != toAdd, "SAME_VALUE");
+        whitelist[target] = toAdd;
+    }
+
+    function transact(
+        address  target,
+        uint     value,
+        bytes    calldata data
+        )
+        external
+        onlyManager
+        returns (
+            bool success,
+            bytes memory returnData
+        )
+    {
+        require(whitelist[target], "INVALID_TARGET");
+        // solium-disable-next-line security/no-call-value
+        (success, returnData) = target.call{value: value}(data);
     }
 }
