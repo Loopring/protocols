fa96d34a4ec26c939bcd47c8f654589415e59c34
kongliangzhong <kongliangzhong@gmail.com>
2020-08-27 20:26:15 +0800

    improve computeAddress.js

--- packages/hebao_v1/computeAddress.js
+++ packages/hebao_v1/computeAddress.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const ethUtil = require("ethereumjs-util");
 
 // const creationCode = "608060405234801561001057600080fd5b506101bc806100206000396000f3fe60806040526004361061002d5760003560e01c80635c60da1b14610078578063d784d426146100a957610034565b3661003457005b600061003e6100de565b90506001600160a01b03811661005357600080fd5b60405136600082376000803683855af43d806000843e818015610074578184f35b8184fd5b34801561008457600080fd5b5061008d6100de565b604080516001600160a01b039092168252519081900360200190f35b3480156100b557600080fd5b506100dc600480360360208110156100cc57600080fd5b50356001600160a01b0316610103565b005b7f49e52b53564741f5cdd331b330c04deb825a37506ec265623007d3f13f9371f85490565b600061010d6100de565b90506001600160a01b03811615610161576040805162461bcd60e51b8152602060048201526013602482015272494e495449414c495a45445f414c524541445960681b604482015290519081900360640190fd5b507f49e52b53564741f5cdd331b330c04deb825a37506ec265623007d3f13f9371f85556fea2646970667358221220111cbc448818bbfa55c66c3c4c62006ccee3717006204ab1d4b2e52803cf4e2864736f6c63430007000033";
@@ -34,55 +35,56 @@ function computeAddress(owner, salt) {
     codeHash
   ]);
 
-  return (
+  const addr =
     "0x" +
     ethUtil
       .keccak(rawBuf)
       .slice(12)
-      .toString("hex")
-  );
+      .toString("hex");
+  return ethUtil.toChecksumAddress(addr);
 }
 
-function findPreciousBlankAddrs(from, to) {
-  function isIdentical(str) {
-    if (str.length < 2) return true;
-    for (let i = 1; i < str.length; i++) {
-      if (str[i] !== str[0]) {
-        return false;
-      }
+function isIdentical(str) {
+  if (str.length < 2) return true;
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] !== str[0]) {
+      return false;
     }
-    return true;
   }
+  return true;
+}
 
-  function calcScore(hexstr) {
-    const len = hexstr.length;
-    if (isIdentical(hexstr.slice(0, 6))) return 8;
-    if (isIdentical(hexstr.slice(0, 5))) return 7;
-    if (isIdentical(hexstr.slice(0, 4))) return 6;
-    if (isIdentical(hexstr.slice(0, 3))) return 5;
-    if (isIdentical(hexstr.slice(0, 2))) return 4;
+function calcScore(hexstr, isTail = false) {
+  let score = 10;
 
-    const arr = Array.from(hexstr);
-    const existed = ["x", "x", "x"];
-    for (const ch of arr) {
-      for (let i = 0; i < existed.length; i++) {
-        if (ch !== existed[i] && existed[i] === "x") {
-          existed[i] = ch;
-          break;
-        }
-      }
+  const len = hexstr.length;
+  if (isIdentical(hexstr.slice(0, 6))) score += 50;
+  else if (isIdentical(hexstr.slice(0, 5))) score += 40;
+  else if (isIdentical(hexstr.slice(0, 4))) score += 30;
+  else if (isIdentical(hexstr.slice(0, 3))) score += 20;
+  else if (isIdentical(hexstr.slice(0, 2))) score += 10;
 
-      if (existed[existed.length - 1] !== "x") break;
-    }
+  if (score > 10 && isTail) score += 10;
 
-    let score = 6;
-    for (let i = existed.length - 1; i > 0; i--) {
-      if (existed[i] !== "x") score -= 2;
-    }
-
-    return score;
+  const arrLower = Array.from(hexstr.toLowerCase());
+  const charSet = new Set();
+  for (const ch of arrLower) {
+    charSet.add(ch);
   }
+  score -= charSet.size;
 
+  const arr = Array.from(hexstr);
+  const charSet2 = new Set();
+  for (const ch of arr) {
+    charSet2.add(ch);
+  }
+  score -= charSet2.size;
+
+  return score;
+}
+
+function findTop50FromRange(from, to) {
+  const result = [];
   for (let i = from; i < to; i++) {
     const addr = computeAddress(zeroAddress, i);
     const prefixScore = calcScore(addr.slice(2, 8));
@@ -91,14 +93,45 @@ function findPreciousBlankAddrs(from, to) {
         .slice(-6)
         .split("")
         .reverse()
-        .join("")
+        .join(""),
+      true
     );
-    const score = prefixScore + tailScore;
-    if (score > 9) {
-      console.log("prefixScore:", prefixScore, "; tailScore:", tailScore);
-      console.log(`salt: ${i}, addr: ${addr}, score: ${score}`);
+    let score = prefixScore + tailScore;
+    if (addr[2] === addr[addr.length - 1]) {
+      score += 20;
+    }
+
+    if (score > 20) {
+      result.push({ score, addr, salt: i });
     }
   }
+
+  console.log(
+    `from: ${from}, to: ${to}, batch length(score > 20): ${result.length}`
+  );
+  result.sort((a, b) => b.score - a.score);
+  return result.slice(0, 50);
+}
+
+function findPreciousBlankAddrs(from, to, resAmount) {
+  let total = [];
+  const step = 10000;
+  for (let i = from; i < to; i += step) {
+    let stepTo = i + step;
+    if (stepTo > to) stepTo = to;
+    const res = findTop50FromRange(i, stepTo);
+    total = total.concat(res);
+  }
+
+  total.sort((a, b) => b.score - a.score);
+  total = total.slice(0, resAmount);
+  console.log("totoal:", total);
+
+  // write to file:
+  const fileName = `blankAddrs-${from}-${to}.json`;
+  fs.writeFileSync(fileName, JSON.stringify(total, undefined, 4));
+
+  return total;
 }
 
 function main() {
@@ -114,4 +147,4 @@ function main() {
 
 // main();
 
-findPreciousBlankAddrs(0, 1000000);
+findPreciousBlankAddrs(0, 1000000, 100);
