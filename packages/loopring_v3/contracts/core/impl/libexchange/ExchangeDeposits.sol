a12c38519c54c3f3de5a9d0d457eb22e8e4feae6
wangdong <wangdong@zhongan.io>
2020-09-02 23:27:25 +0800

    more

--- packages/loopring_v3/contracts/core/impl/libexchange/ExchangeDeposits.sol
+++ packages/loopring_v3/contracts/core/impl/libexchange/ExchangeDeposits.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2017 Loopring Technology Limited. pragma solidity ^0.7.0;
+// Copyright 2017 Loopring Technology Limited.
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
