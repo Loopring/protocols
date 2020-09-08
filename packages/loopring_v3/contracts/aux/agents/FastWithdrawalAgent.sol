f00faf33b78e79838f4438773c946abf8ac470dc
Brechtpd <Brechtp.Devos@gmail.com>
2020-09-08 19:01:25 +0200

    [protocol 3.6] Alternative fast withdrawal implementation

--- packages/loopring_v3/contracts/aux/agents/FastWithdrawalAgent.sol
+++ packages/loopring_v3/contracts/aux/agents/FastWithdrawalAgent.sol
@@ -11,9 +11,8 @@ import "../../lib/MathUint.sol";
 import "../../lib/ReentrancyGuard.sol";
 import "../../lib/SignatureUtil.sol";
 
-
-/// @title Fast withdrawal agent implementation. The fast withdrawal request reduces to
-///        a normal onchain withdrawal request after a specified time limit has exceeded.
+/// @title Fast withdrawal agent implementation. With the help of liquidity providers (LPs),
+///        exchange operators can convert any normal withdrawals into fast withdrawals.
 ///
 ///        Fast withdrawals are a way for the owner to provide instant withdrawals for
 ///        users with the help of a liquidity provider and conditional transfers.
@@ -21,14 +20,9 @@ import "../../lib/SignatureUtil.sol";
 ///        A fast withdrawal requires the non-trustless cooperation of 2 parties:
 ///        - A liquidity provider which provides funds to users immediately onchain
 ///        - The operator which will make sure the user has sufficient funds offchain
-///          so that the liquidity provider can be paid back offchain using a conditional transfer.
-///          The operator also needs to process those conditional transfers so that the
-///          liquidity provider receives its funds back in its own account where it
-///          again has full custody over it.
-///
-///        However, there is a special case when the fast withdrawal reduces to a standard
-///        withdrawal and the fee is paid onchain. In this case the withdrawal can be
-///        done completely trustless, no cooperation with the owner is needed.
+///          so that the liquidity provider can be paid back.
+///          The operator also needs to process those withdrawals so that the
+///          liquidity provider receives its funds back.
 ///
 ///        We require the fast withdrawals to be executed by the liquidity provider (as msg.sender)
 ///        so that the liquidity provider can impose its own rules on how its funds are spent. This will
@@ -44,50 +38,58 @@ import "../../lib/SignatureUtil.sol";
 ///        authorize this contract as their agent.
 ///
 /// @author Brecht Devos - <brecht@loopring.org>
+/// @author Kongliang Zhong - <kongliang@loopring.org>
+/// @author Daniel Wang - <daniel@loopring.org>
 contract FastWithdrawalAgent is ReentrancyGuard
 {
     using AddressUtil       for address;
     using AddressUtil       for address payable;
     using ERC20SafeTransfer for address;
     using MathUint          for uint;
-    using SignatureUtil     for bytes32;
 
-    struct FastWithdrawal
+    event Processed(
+        address exchange,
+        address from,
+        address to,
+        address token,
+        uint96  amount,
+        address provider,
+        bool    success
+    );
+
+    struct Withdrawal
     {
         address exchange;
         address from;                   // The owner of the account
-        address to;                     // The address that will receive the tokens withdrawn
+        address to;                     // The `to` address of the withdrawal
         address token;
         uint96  amount;
-        address feeToken;
-        uint96  fee;
-        uint32  nonce;
-        uint32  validUntil;
-
-        bytes   signature;
-    }
-
-    // EIP712
-    bytes32 constant public FASTWITHDRAWAL_TYPEHASH = keccak256(
-        "FastWithdrawal(address exchange,address from,address to,address token,uint96 amount,address feeToken,uint96 fee,uint32 nonce,uint32 validUntil)"
-    );
-    bytes32 public DOMAIN_SEPARATOR;
-
-    constructor()
-    {
-        DOMAIN_SEPARATOR = EIP712.hash(EIP712.Domain("FastWithdrawalAgent", "1.0", address(this)));
+        uint32  storageID;
     }
 
     // This method needs to be called by any liquidity provider
-    function executeFastWithdrawals(FastWithdrawal[] memory fastWithdrawals)
+    function execute(Withdrawal[] calldata withdrawals)
         public
         nonReentrant
         payable
     {
         // Do all fast withdrawals
-        for (uint i = 0; i < fastWithdrawals.length; i++) {
-            executeFastWithdrawal(fastWithdrawals[i]);
+        for (uint i = 0; i < withdrawals.length; i++) {
+            executeInternal(withdrawals[i]);
         }
+        // Return any ETH left into this contract
+        // (can happen when more ETH is sent than needed for the fast withdrawals)
+        msg.sender.sendETHAndVerify(address(this).balance, gasleft());
+    }
+
+    // This method needs to be called by any liquidity provider
+    function execute(Withdrawal calldata withdrawal)
+        public
+        nonReentrant
+        payable
+    {
+        // Do the fast withdrawal
+        executeInternal(withdrawal);
         // Return any ETH left into this contract
         // (can happen when more ETH is sent than needed for the fast withdrawals)
         msg.sender.sendETHAndVerify(address(this).balance, gasleft());
@@ -95,68 +97,51 @@ contract FastWithdrawalAgent is ReentrancyGuard
 
     // -- Internal --
 
-    function executeFastWithdrawal(FastWithdrawal memory fastWithdrawal)
+    function executeInternal(Withdrawal calldata withdrawal)
         internal
     {
+        require(
+            withdrawal.exchange != address(0) &&
+            withdrawal.from != address(0) &&
+            withdrawal.to != address(0) &&
+            withdrawal.amount != 0,
+            "INVALID_WITHDRAWAL"
+        );
+
         // The liquidity provider always authorizes the fast withdrawal by being the direct caller
         address payable liquidityProvider = msg.sender;
 
-        if (fastWithdrawal.signature.length > 0) {
-            // Compute the hash
-            bytes32 hash = EIP712.hashPacked(
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encodePacked(
-                        FASTWITHDRAWAL_TYPEHASH,
-                        fastWithdrawal.exchange,
-                        fastWithdrawal.from,
-                        fastWithdrawal.to,
-                        fastWithdrawal.token,
-                        fastWithdrawal.amount,
-                        fastWithdrawal.feeToken,
-                        fastWithdrawal.fee,
-                        fastWithdrawal.nonce,
-                        fastWithdrawal.validUntil
-                    )
-                )
-            );
-
-            // Check the signature
-            require(hash.verifySignature(fastWithdrawal.from, fastWithdrawal.signature), "INVALID_SIGNATURE");
-
-            // Check the time limit
-            require(block.timestamp <= fastWithdrawal.validUntil, "TX_EXPIRED");
-
-            // Approve the offchain transfer from the account that's withdrawing back to the liquidity provider
-            IExchangeV3(fastWithdrawal.exchange).approveOffchainTransfer(
-                fastWithdrawal.from,
+        bool success;
+        // Override the destination address of a withdrawal to the address of the liquidity provider
+        try IExchangeV3(withdrawal.exchange).setWithdrawalRecipient(
+            withdrawal.from,
+            withdrawal.to,
+            withdrawal.token,
+            withdrawal.amount,
+            withdrawal.storageID,
+            liquidityProvider
+        ) {
+            // Transfer the tokens immediately to the requested address
+            // using funds from the liquidity provider (`msg.sender`).
+            transfer(
                 liquidityProvider,
-                fastWithdrawal.token,
-                fastWithdrawal.amount,
-                fastWithdrawal.feeToken,
-                fastWithdrawal.fee,
-                fastWithdrawal.validUntil,
-                fastWithdrawal.nonce
+                withdrawal.to,
+                withdrawal.token,
+                withdrawal.amount
             );
-        } else {
-            // Override the destination address of a withdrawal to the address of the liquidity provider
-            IExchangeV3(fastWithdrawal.exchange).setWithdrawalRecipient(
-                fastWithdrawal.from,
-                fastWithdrawal.to,
-                fastWithdrawal.token,
-                fastWithdrawal.amount,
-                fastWithdrawal.nonce,
-                liquidityProvider
-            );
+            success = true;
+        } catch {
+            success = false;
         }
 
-        // Transfer the tokens immediately to the requested address
-        // using funds from the liquidity provider (`msg.sender`).
-        transfer(
+        emit Processed(
+            withdrawal.exchange,
+            withdrawal.from,
+            withdrawal.to,
+            withdrawal.token,
+            withdrawal.amount,
             liquidityProvider,
-            fastWithdrawal.to,
-            fastWithdrawal.token,
-            fastWithdrawal.amount
+            success
         );
     }
 
