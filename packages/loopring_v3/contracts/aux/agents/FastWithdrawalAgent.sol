33f4f693aa963e3437ad6b66765122c05458ff06
kongliangzhong <kongliangzhong@gmail.com>
2020-09-08 17:30:11 +0800

    [protocol36]FastWithdrawal impl

--- packages/loopring_v3/contracts/aux/agents/FastWithdrawalAgent.sol
+++ packages/loopring_v3/contracts/aux/agents/FastWithdrawalAgent.sol
@@ -12,65 +12,64 @@ import "../../lib/ReentrancyGuard.sol";
 import "../../lib/SignatureUtil.sol";
 
 
-/// @title Fast withdrawal agent implementation. The fast withdrawal request reduces to
-///        a normal onchain withdrawal request after a specified time limit has exceeded.
+/// @title Fast withdrawal agent implementation.
+///        With the help of liquidity providers (LPs), exchange operators can convert any
+///        normal withdrawals into fast withdrawals. This design, however, requires LPs
+///        to trust the exchange operators for providing real user withdrawal information.
 ///
-///        Fast withdrawals are a way for the owner to provide instant withdrawals for
-///        users with the help of a liquidity provider and conditional transfers.
+///        LPs can send transactions to tranfer Ether/tokens to exchange users with valid
+///        FastWithdrawalApprovals. These users can also send overriding transactions with
+///        the same FastWithdrawalApprovals using a higher gas price to further accelerate
+///        the incoming transfers.
 ///
-///        A fast withdrawal requires the non-trustless cooperation of 2 parties:
-///        - A liquidity provider which provides funds to users immediately onchain
-///        - The operator which will make sure the user has sufficient funds offchain
-///          so that the liquidity provider can be paid back offchain using a conditional transfer.
-///          The operator also needs to process those conditional transfers so that the
-///          liquidity provider receives its funds back in its own account where it
-///          again has full custody over it.
-///
-///        However, there is a special case when the fast withdrawal reduces to a standard
-///        withdrawal and the fee is paid onchain. In this case the withdrawal can be
-///        done completely trustless, no cooperation with the owner is needed.
-///
-///        We require the fast withdrawals to be executed by the liquidity provider (as msg.sender)
-///        so that the liquidity provider can impose its own rules on how its funds are spent. This will
-///        inevitably need to be done in close cooperation with the operator, or by the operator
-///        itself using a smart contract where the liquidity provider enforces who, how
-///        and even if their funds can be used to facilitate the fast withdrawals.
-///
-///        The liquidity provider can call `executeFastWithdrawals` to provide users
-///        immediately with funds onchain. This allows the security of the funds to be handled
-///        by any EOA or smart contract.
-///
-///        Users that want to make use of this functionality have to
-///        authorize this contract as their agent.
+///        Users that want to make use of this functionality have to authorize this contract
+///        as their agent.
 ///
 /// @author Brecht Devos - <brecht@loopring.org>
+/// @author Kongliang Zhong - <kongliang@loopring.org>
+/// @author Daniel Wang - <daniel@loopring.org>
 contract FastWithdrawalAgent is ReentrancyGuard
 {
     using AddressUtil       for address;
     using AddressUtil       for address payable;
     using ERC20SafeTransfer for address;
-    using MathUint          for uint;
     using SignatureUtil     for bytes32;
 
-    struct FastWithdrawal
+    enum Status {
+        SUCCEEDED,
+        FRONTRUN,
+        EXPIRED,
+        TOO_LATE
+    }
+
+    event Processed(
+        address exchange,
+        address from,
+        address to,
+        address token,
+        uint96  amount,
+        address provider,
+        address transactor,
+        Status  status
+    );
+
+    struct FastWithdrawalApproval
     {
         address exchange;
         address from;                   // The owner of the account
         address to;                     // The address that will receive the tokens withdrawn
         address token;
         uint96  amount;
-        address feeToken;
-        uint96  fee;
-        uint32  nonce;
-        uint32  validUntil;
-
-        bytes   signature;
+        uint32  storageID;
+        uint64  validUntil; // most significant 32 bits as block height, least significant 32 bits as block time
+        address provider;
+        bytes   signature; // provider's signature
     }
 
-    // EIP712
-    bytes32 constant public FASTWITHDRAWAL_TYPEHASH = keccak256(
-        "FastWithdrawal(address exchange,address from,address to,address token,uint96 amount,address feeToken,uint96 fee,uint32 nonce,uint32 validUntil)"
+    bytes32 constant public FASTWITHDRAWAL_APPROVAL_TYPEHASH = keccak256(
+        "FastWithdrawalApproval(address exchange,address from,address to,address token,uint96 amount,uint32 storageID,uint64 validUntil)"
     );
+
     bytes32 public DOMAIN_SEPARATOR;
 
     constructor()
@@ -78,86 +77,103 @@ contract FastWithdrawalAgent is ReentrancyGuard
         DOMAIN_SEPARATOR = EIP712.hash(EIP712.Domain("FastWithdrawalAgent", "1.0", address(this)));
     }
 
-    // This method needs to be called by any liquidity provider
-    function executeFastWithdrawals(FastWithdrawal[] memory fastWithdrawals)
-        public
+    function execute(FastWithdrawalApproval memory fwa)
+        external
         nonReentrant
         payable
     {
-        // Do all fast withdrawals
-        for (uint i = 0; i < fastWithdrawals.length; i++) {
-            executeFastWithdrawal(fastWithdrawals[i]);
+        Status status = execute_(fwa);
+        emit Processed(
+            fwa.exchange,
+            fwa.from,
+            fwa.to,
+            fwa.token,
+            fwa.amount,
+            fwa.provider,
+            msg.sender,
+            status
+        );
+
+        msg.sender.sendETHAndVerify(address(this).balance, gasleft());
+    }
+
+    function execute(FastWithdrawalApproval[] memory fwas)
+        external
+        nonReentrant
+        payable
+    {
+        Status status;
+        for (uint i = 0; i < fwas.length; i++) {
+            status = execute_(fwas[i]);
+            emit Processed(
+                fwas[i].exchange,
+                fwas[i].from,
+                fwas[i].to,
+                fwas[i].token,
+                fwas[i].amount,
+                fwas[i].provider,
+                msg.sender,
+                status
+            );
         }
-        // Return any ETH left into this contract
-        // (can happen when more ETH is sent than needed for the fast withdrawals)
         msg.sender.sendETHAndVerify(address(this).balance, gasleft());
     }
 
     // -- Internal --
 
-    function executeFastWithdrawal(FastWithdrawal memory fastWithdrawal)
+    function execute_(FastWithdrawalApproval memory fwa)
         internal
+        returns (Status)
     {
-        // The liquidity provider always authorizes the fast withdrawal by being the direct caller
-        address payable liquidityProvider = msg.sender;
+        require(
+            fwa.exchange != address(0) &&
+            fwa.from != address(0) &&
+            fwa.to != address(0) &&
+            fwa.amount != 0,
+            "INVALID_WITHDRAWAL"
+        );
 
-        if (fastWithdrawal.signature.length > 0) {
-            // Compute the hash
-            bytes32 hash = EIP712.hashPacked(
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encodePacked(
-                        FASTWITHDRAWAL_TYPEHASH,
-                        fastWithdrawal.exchange,
-                        fastWithdrawal.from,
-                        fastWithdrawal.to,
-                        fastWithdrawal.token,
-                        fastWithdrawal.amount,
-                        fastWithdrawal.feeToken,
-                        fastWithdrawal.fee,
-                        fastWithdrawal.nonce,
-                        fastWithdrawal.validUntil
-                    )
+        // Compute the hash
+        bytes32 hash = EIP712.hashPacked(
+            DOMAIN_SEPARATOR,
+            keccak256(
+                abi.encodePacked(
+                    FASTWITHDRAWAL_APPROVAL_TYPEHASH,
+                    fwa.exchange,
+                    fwa.from,
+                    fwa.to,
+                    fwa.token,
+                    fwa.amount,
+                    fwa.storageID,
+                    fwa.validUntil
                 )
-            );
+            )
+        );
 
-            // Check the signature
-            require(hash.verifySignature(fastWithdrawal.from, fastWithdrawal.signature), "INVALID_SIGNATURE");
+        // Check the signature
+        require(hash.verifySignature(fwa.provider, fwa.signature ), "INVALID_SIGNATURE");
+        require(checkValidUntil(fwa.validUntil), "FASTWITHDRAWAL_APPROVAL_EXPIRED");
 
-            // Check the time limit
-            require(block.timestamp <= fastWithdrawal.validUntil, "TX_EXPIRED");
-
-            // Approve the offchain transfer from the account that's withdrawing back to the liquidity provider
-            IExchangeV3(fastWithdrawal.exchange).approveOffchainTransfer(
-                fastWithdrawal.from,
-                liquidityProvider,
-                fastWithdrawal.token,
-                fastWithdrawal.amount,
-                fastWithdrawal.feeToken,
-                fastWithdrawal.fee,
-                fastWithdrawal.validUntil,
-                fastWithdrawal.nonce
-            );
-        } else {
-            // Override the destination address of a withdrawal to the address of the liquidity provider
-            IExchangeV3(fastWithdrawal.exchange).setWithdrawalRecipient(
-                fastWithdrawal.from,
-                fastWithdrawal.to,
-                fastWithdrawal.token,
-                fastWithdrawal.amount,
-                fastWithdrawal.nonce,
-                liquidityProvider
-            );
+        try IExchangeV3(fwa.exchange).setWithdrawalRecipient(
+            fwa.from,
+            fwa.to,
+            fwa.token,
+            fwa.amount,
+            fwa.storageID,
+            fwa.provider
+        ) {} catch {
+            return Status.TOO_LATE;
         }
-
         // Transfer the tokens immediately to the requested address
         // using funds from the liquidity provider (`msg.sender`).
         transfer(
-            liquidityProvider,
-            fastWithdrawal.to,
-            fastWithdrawal.token,
-            fastWithdrawal.amount
+            fwa.provider,
+            fwa.to,
+            fwa.token,
+            fwa.amount
         );
+
+        return Status.SUCCEEDED;
     }
 
     function transfer(
@@ -175,5 +191,14 @@ contract FastWithdrawalAgent is ReentrancyGuard
                 token.safeTransferFromAndVerify(from, to, amount);  // ERC20 token
             }
         }
+    }
+
+    function checkValidUntil(uint64 validUntil)
+        internal
+        view
+        returns (bool)
+    {
+        return (validUntil & 0xFFFFFFFF) >= block.timestamp ||
+            (validUntil >> 32) >= block.number;
     }
 }
