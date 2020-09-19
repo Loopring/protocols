02290a0c42b8a156c3e13fc0eaa6b728715dec99
Daniel Wang <daniel@loopring.org>
2020-09-19 11:06:16 +0800

    auto format

--- packages/loopring_v3/circuit/Circuits/AccountUpdateCircuit.h
+++ packages/loopring_v3/circuit/Circuits/AccountUpdateCircuit.h
@@ -99,12 +99,7 @@ class AccountUpdateCircuit : public BaseTransactionCircuit
             validUntil.packed,
             NUM_BITS_TIMESTAMP,
             FMT(prefix, ".requireValidUntil")),
-          requireValidFee(
-            pb,
-            fee.packed,
-            maxFee.packed,
-            NUM_BITS_AMOUNT,
-            FMT(prefix, ".requireValidFee")),
+          requireValidFee(pb, fee.packed, maxFee.packed, NUM_BITS_AMOUNT, FMT(prefix, ".requireValidFee")),
 
           // Type
           isConditional(pb, type.packed, ".isConditional"),
