f88a6b8cf35f7e95022f1c010a27b86caa172b98
wangdong <wangdong@zhongan.io>
2020-09-27 12:32:12 +0800

    more

--- packages/loopring_v3/contracts/amm/LoopringAmmFactory.sol
+++ packages/loopring_v3/contracts/amm/LoopringAmmFactory.sol
@@ -3,21 +3,21 @@
 pragma solidity ^0.7.0;
 pragma experimental ABIEncoderV2;
 
-import "../thirdparty/Create2.sol";
+import "../lib/Create2.sol";
 import "../lib/OwnerManagable.sol";
+import "../lib/ReentrancyGuard.sol";
 import "../lib/SimpleProxy.sol";
 import "./LoopringAmmPool.sol";
 import "./libamm/AmmData.sol";
 
-contract LoopringAmmFactory is OwnerManagable
+
+contract LoopringAmmFactory is OwnerManagable, ReentrancyGuard
 {
     event PoolCreated(AmmData.PoolConfig config);
 
     address public poolImplementation;
 
-    constructor(
-        address _poolImplementation
-        )
+    constructor(address _poolImplementation)
         OwnerManagable()
     {
         poolImplementation = _poolImplementation;
@@ -28,12 +28,28 @@ contract LoopringAmmFactory is OwnerManagable
         )
         external
         onlyManager
-        returns (address pool)
+        nonReentrant
+        returns (address payable pool)
     {
-        pool = address(new SimpleProxy(poolImplementation));
+        bytes32 salt = keccak256(
+            abi.encodePacked(
+                config.exchange,
+                config.poolName,
+                config.accountID,
+                config.tokens,
+                config.weights,
+                config.feeBips,
+                config.tokenSymbol
+            )
+        );
+
+        pool = Create2.deploy(salt, type(SimpleProxy).creationCode);
+
+        SimpleProxy proxy = SimpleProxy(pool);
+        proxy.setImplementation(poolImplementation);
+
         LoopringAmmPool(pool).setupPool(config);
 
         emit PoolCreated(config);
     }
 }
-
