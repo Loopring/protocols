47a4796104d0f1e7d38c22e848591ddb65a2e388
Daniel Wang <daniel@loopring.org>
2020-10-04 10:44:03 +0800

    remove totalSupply variable

--- packages/loopring_v3/contracts/amm/libamm/AmmStatus.sol
+++ packages/loopring_v3/contracts/amm/libamm/AmmStatus.sol
@@ -72,9 +72,9 @@ library AmmStatus
         }
 
         // Mint all liquidity tokens to the pool account on L2
-        state.balanceOf[address(this)] = AmmData.POOL_TOKEN_MINTED_SUPPLY();
+        S.balanceOf[address(this)] = AmmData.POOL_TOKEN_MINTED_SUPPLY();
         S.poolTokenBurnedSupply = AmmData.POOL_TOKEN_MINTED_SUPPLY();
-        S.approve(address(exchange.getDepositContract()), uint96(-1));
+        S.approve(address(exchange.getDepositContract()), uint(-1));
         exchange.deposit(
             address(this), // from
             address(this), // to
