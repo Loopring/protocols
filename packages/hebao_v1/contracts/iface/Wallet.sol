de569b8fe5947f4135a2bb11027cd3f9d29632d5
Daniel Wang <daniel@loopring.org>
2020-10-21 14:58:45 +0800

    modify Wallet.sol comment

--- packages/hebao_v1/contracts/iface/Wallet.sol
+++ packages/hebao_v1/contracts/iface/Wallet.sol
@@ -60,9 +60,10 @@ interface Wallet
     ///
     ///      This method will emit `Transacted` event if it doesn't throw.
     ///
-    ///      Note: this method must ONLY allow invocations from a module that has
+    ///      Note: 1) this method must ONLY allow invocations from a module that has
     ///      been added to this wallet. The wallet owner shall NOT be permitted
-    ///      to call this method directly.
+    ///      to call this method directly. 2) Reentrancy inside this function should
+    ///      NOT cause any problems.
     ///
     /// @param mode The transaction mode, 1 for CALL, 2 for DELEGATECALL.
     /// @param to The desitination address.
