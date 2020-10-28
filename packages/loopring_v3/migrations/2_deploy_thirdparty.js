67543d192cefc0461cf84703537f1dbcd58a4f6b
kongliangzhong <kongliangzhong@gmail.com>
2020-10-28 16:01:30 +0800

    [loopring36] update migration scripts and add a getter method for VK (#1968)
    * [loopring36] update migration scripts and add a getter method for VK
    
    * Update BlockVerifier.sol
    
    Co-authored-by: Daniel Wang <daniel@loopring.org>

--- packages/loopring_v3/migrations/2_deploy_thirdparty.js
+++ packages/loopring_v3/migrations/2_deploy_thirdparty.js
@@ -1,9 +1,7 @@
-const Cloneable = artifacts.require("Cloneable");
 const BatchVerifier = artifacts.require("BatchVerifier");
 
 module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
-    await deployer.deploy(Cloneable);
     await deployer.deploy(BatchVerifier);
   });
 };
