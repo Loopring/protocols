47cf53ab90748160ebfbd63ae7a30e84ee9c972a
kongliangzhong <kongliangzhong@gmail.com>
2020-11-03 20:46:32 +0800

    update build-all-flattened.sh

--- packages/hebao_v1/deployment/build-all-flattened.sh
+++ packages/hebao_v1/deployment/build-all-flattened.sh
@@ -17,11 +17,11 @@ echo "build all flattened contracts in dir $1/flattened ..."
 
 ##cd $1
 
-SOLC_COMMAND="../../node_modules/.bin/solcjs"
+SOLC_COMMAND="solc"
 
 for contract in flattened/*; do
     echo "generate abi for $contract ..."
-    $SOLC_COMMAND -o $OUT_DIR --abi $contract
+    $SOLC_COMMAND -o $OUT_DIR --optimize --abi $contract
     echo "generate binary for $contract ..."
-    $SOLC_COMMAND -o $OUT_DIR --optimize --bin $contract
+    $SOLC_COMMAND -o $OUT_DIR --optimize --optimize-runs 999999 --bin $contract
 done
