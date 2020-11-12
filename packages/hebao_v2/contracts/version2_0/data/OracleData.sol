e3c83495ce6df3991de68e75ad7605c93e5f129d
kongliangzhong <kongliangzhong@gmail.com>
2020-11-12 12:47:45 +0800

    add all contracts from Dong's PR

---
+++ packages/hebao_v2/contracts/version2_0/data/OracleData.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2017 Loopring Technology Limited.
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "../../base/WalletDataLayout.sol";
+import "../../iface/IPriceOracle.sol";
+
+/// @title OracleData
+/// @author Daniel Wang - <daniel@loopring.org>
+library OracleData
+{
+    function priceOracle(WalletDataLayout.State storage S)
+        internal
+        view
+        returns (IPriceOracle)
+    {
+
+    }
+
+    function setPriceOracle(
+        WalletDataLayout.State storage S,
+        IPriceOracle oracle
+        )
+        internal
+    {
+    }
+}
---
+++ packages/hebao_v2/contracts/version2_0/modules/ERC1271Module.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2017 Loopring Technology Limited.
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "../../lib/ERC1271.sol";
+import "../../lib/SignatureUtil.sol";
+import "../../base/Module.sol";
+import "../../base/WalletDataLayout.sol";
+import "../data/SecurityData.sol";
+
+
+/// @title ERC1271Module
+/// @dev This module enables our smart wallets to message signers.
+/// @author Brecht Devos - <brecht@loopring.org>
+/// @author Daniel Wang - <daniel@loopring.org>
+contract ERC1271Module is ERC1271, Module
+{
+    using SignatureUtil for bytes32;
+    using SecurityData  for WalletDataLayout.State;
+
+    function bindableMethods()
+        public
+        override
+        pure
+        returns (bytes4[] memory methods)
+    {
+        methods = new bytes4[](1);
+        methods[0] = this.isValidSignature.selector;
+    }
+
+    function isValidSignature(
+        bytes32      _signHash,
+        bytes memory _signature
+        )
+        public
+        override
+        view
+        returns (bytes4 magicValue)
+    {
+        if (state.isLocked() || !_signHash.verifySignature(thisWallet().owner(), _signature)) {
+            return 0;
+        } else {
+            return ERC1271_MAGICVALUE;
+        }
+    }
+}
