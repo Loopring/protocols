0926e76e64100f9ea3d7c6979f049290f8133889
kongliangzhong <kongliangzhong@gmail.com>
2020-11-12 12:27:18 +0800

    add hebao_v2, using hardhat framework

---
+++ packages/hebao_v2/README.md
@@ -0,0 +1,13 @@
+# Hebao V2
+
+## compile
+
+> npx hardhat compile
+
+## test
+
+> npx hardhat test
+
+## run scripts:
+
+> e.g. npx hardhat run scripts/sample-script.js
---
+++ packages/hebao_v2/hardhat.config.ts
@@ -0,0 +1,22 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3"
+};
