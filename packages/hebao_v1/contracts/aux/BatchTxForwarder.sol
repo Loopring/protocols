13dde36fb0d0fa8622fb96276ed72802110dabf5
kongliangzhong <kongliangzhong@gmail.com>
2020-12-01 14:18:34 +0800

    feebacks

--- packages/hebao_v1/contracts/aux/BatchTxForwarder.sol
+++ packages/hebao_v1/contracts/aux/BatchTxForwarder.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2017 Loopring Technology Limited.
 pragma solidity ^0.7.0;
-pragma experimental ABIEncoderV2;
 
 import "../lib/Claimable.sol";
 import "../lib/Drainable.sol";
@@ -11,10 +10,15 @@ import "../lib/Drainable.sol";
 /// @author Daniel Wang - <daniel@loopring.org>
 contract BatchTxForwarder is Drainable, Claimable
 {
-    constructor() Drainable() Claimable() {}
+   public immutable address target;
+    constructor(address _target)
+        Claimable()
+        Drainable()
+    {
+        target = _target;
+    }
 
     function transactTo(
-        address to,
         bytes[] calldata txs,
         uint[]  calldata gasLimits
         )
@@ -23,8 +27,12 @@ contract BatchTxForwarder is Drainable, Claimable
         require(txs.length == gasLimits.length, "SIZE_DIFF");
 
         for (uint i = 0; i < txs.length; i++) {
-            (bool success,) = to.call{gas: gasLimits[i]}(txs[i]);
-            require(success, "TX_FAILED");
+            (bool success,) = target.call{gas: gasLimits[i]}(txs[i]);
+            if (!success) {
+                assembly {
+                    revert(add(returnData, 32), mload(returnData))
+                }
+            }
         }
     }
 
