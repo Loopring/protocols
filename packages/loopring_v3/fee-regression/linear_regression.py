3be4d77d0b37c690a5b29dc73452398328c83ed8
wangdong <wangdong@zhongan.io>
2021-02-10 12:20:21 +0800

    need to calculate the fees from a business perspective

--- packages/loopring_v3/fee-regression/linear_regression.py
+++ packages/loopring_v3/fee-regression/linear_regression.py
@@ -51,3 +51,58 @@ print("- account_update_cost: " + str(regressor.coef_[3] + offset))
 print("- amm_update_cost:     " + str(regressor.coef_[4] + offset))
 print("- signature_cost:      " + str(regressor.coef_[5] + offset))
 '''
+
+global_fee_discount = 0; # a discount for all kinds of fees
+lrc_discount = 0.2;      # if LRC is used as fees (for transfers and trades)
+protocol_fee = 0.2;      # only for transfers and trades
+affiliate_reward = 0.2;  # only for trades
+
+
+# Read or calculated from data set
+num_noop 			= 1
+num_deposit 		= 2;
+num_withdrawal 		= 3;
+num_transfer 		= 4;
+num_trade 			= 5;
+num_amm_update 		= 6;
+num_signature 		= 7;
+
+# The total gas cost
+total_cost = num_noop * cost_noop +
+	num_deposit * cost_deposit +
+	num_withdrawal * cost_withdrawal +
+	num_transfer * cost_transfer +
+	num_trade * cost_trade +
+	num_amm_update * cost_amm_update +
+	num_signature *c ost_signature
+
+
+num_amm_join_exit = num_amm_update / 2
+cost_amm_join_exit = cost_amm_update * 2 + cost_transfer * 3;
+
+# Get the real transfer counts (AMM excluded)
+num_transfer -= num_amm_join_exit * 3
+
+# What if we only charge for these fore types of tx
+chargable_cost =  num_withdrawal * cost_withdrawal +
+	num_transfer * cost_transfer +
+	num_trade * cost_trade +
+	num_amm_join_exit * cost_amm_join_exit;
+
+
+# We will suffer a loss if we don't apply this ratio
+ratio = (1 - global_fee_discount) * total_cost / chargable_cost;
+
+# Increase each cost by the ratio
+cost_withdrawal *= ratio;
+cost_transfer *= ratio;
+cost_trade *= ratio;
+cost_amm_join_exit *= ratio;
+
+
+cost_transfer /= ((1 - lrc_discount) * (1 - protocol_fee))
+cost_trade /= ((1 - lrc_discount) * (1 - protocol_fee) * (1 - affiliate_reward))
+
+
+
+# print cost...
