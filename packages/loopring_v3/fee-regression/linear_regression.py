011d04ebc175deffbdc4ac608bd69b8b490b7988
Brechtpd <Brechtp.Devos@gmail.com>
2021-02-10 19:32:32 +0100

    Improvements

--- packages/loopring_v3/fee-regression/linear_regression.py
+++ packages/loopring_v3/fee-regression/linear_regression.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import math
 
-global_fee_discount = 0.0;  # a discount for all kinds of fees
-lrc_discount = 0.2;         # if LRC is used as fees (for transfers and trades)
-protocol_fee = 0.2;         # only for transfers and trades
-affiliate_reward = 0.2;     # only for trades
+global_fee_discount = 0.0  # a discount for all kinds of fees
+lrc_discount = 0.2         # if LRC is used as fees (for transfers and trades)
+protocol_fee = 0.2         # only for transfers and trades
+affiliate_reward = 0.2     # only for trades
+transfer_cost_cap = 700    # caps transfer costs and moves any excesses to trades
+
+block_size = 386
 
 dataset = pd.read_csv('block_stats.csv')
 dataset.head()
@@ -15,12 +18,18 @@ dataset.head()
 X = dataset.iloc[:,2:].values
 y = dataset.iloc[:,1].values
 
+noops = dataset.iloc[:,2].values
+deposits = dataset.iloc[:,3].values
+withdrawals = dataset.iloc[:,4].values
+transfers = dataset.iloc[:,5].values
+trades = dataset.iloc[:,6].values
+account_updates = dataset.iloc[:,7].values
+amm_updates = dataset.iloc[:,8].values
+signatures = dataset.iloc[:,9].values
+
 # apply global discount
 for i in range(len(y)):
     y[i] *= (1.0 - global_fee_discount)
-
-#print(X)
-#print(y)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -37,18 +46,7 @@ y_pred = regressor.predict(X_test)
 #print(regressor.intercept_)
 
 # distribute fixed cost over all transactions equally
-offset = regressor.intercept_/386
-
-'''
-print("- noop_cost:           " + str(regressor.coef_[0] + offset))
-print("- deposit_cost:        " + str(regressor.coef_[1] + offset))
-print("- withdrawal_cost:     " + str(regressor.coef_[2] + offset))
-print("- transfer_cost:       " + str(regressor.coef_[3] + offset))
-print("- trade_cost:          " + str(regressor.coef_[4] + offset))
-print("- account_update_cost: " + str(regressor.coef_[5] + offset))
-print("- amm_update_cost:     " + str(regressor.coef_[6] + offset))
-print("- signature_cost:      " + str(regressor.coef_[7] + offset))
-'''
+offset = regressor.intercept_/block_size
 
 # raw costs
 noop_cost = regressor.coef_[0] + offset
@@ -60,26 +58,16 @@ account_update_cost = regressor.coef_[5] + offset
 amm_update_cost = regressor.coef_[6] + offset
 signature_cost = regressor.coef_[7] + offset
 
+# Calculcates probability for a transaction type
+def p(v):
+    total = 0
+    for e in v:
+        total += e
+    return total/len(v)/block_size
 
-# Calculate the chance a transaction is a noop
-noops = dataset.iloc[:,2].values
-total_noops = 0
-for e in noops:
-    total_noops += e
-p_noop = total_noops/len(noops)/386
-
-# Calculate the chance a transaction is a deposit
-deposits = dataset.iloc[:,3].values
-total_deposits = 0
-for e in deposits:
-    total_deposits += e
-p_deposit = total_deposits/len(deposits)/386
-
-#print(p_noop)
-#print(p_deposit)
-
+# Move costs for noops and deposits to other transactions
 # calculate the extra cost needed on all other transactions to pay for these transactions
-extra_cost = noop_cost * p_noop + deposit_cost * p_deposit
+extra_cost = noop_cost * p(noops) + deposit_cost * p(deposits)
 #print(extra_cost)
 
 # Distribute extra cost over all other transactions
@@ -89,7 +77,6 @@ trade_cost += extra_cost
 account_update_cost += extra_cost
 amm_update_cost += extra_cost
 signature_cost += extra_cost
-
 
 # AMM join/exit
 amm_join_exit_cost = 2 * amm_update_cost + 3 * transfer_cost + 1 * signature_cost
@@ -101,11 +88,23 @@ transfer_cost /= ((1 - lrc_discount) * (1 - protocol_fee))
 trade_cost /= ((1 - lrc_discount) * (1 - protocol_fee) * (1 - affiliate_reward))
 
 
-def round(v):
+# Apply transfer cap
+# Calculate excess
+excess = transfer_cost - transfer_cost_cap
+if excess > 0:
+    # get the transfers not part of an AMM join/exit
+    transfers_no_amm = transfers - (amm_updates * 1.5)
+    # Add the excess to trades with a transfer/trade ratio
+    trade_cost += excess * (p(transfers_no_amm)/p(trades))
+    # Set the cap
+    transfer_cost = transfer_cost_cap
+
+
+def round_(v):
     return math.ceil(v / 50) * 50
 
-print("- withdrawal_cost:     " + str(round(withdrawal_cost)))
-print("- transfer_cost:       " + str(round(transfer_cost)))
-print("- trade_cost:          " + str(round(trade_cost)))
-print("- account_update_cost: " + str(round(account_update_cost)))
-print("- amm_join_exit_cost:  " + str(round(amm_join_exit_cost)))
+print("- withdrawal_cost:     " + str(round_(withdrawal_cost)))
+print("- transfer_cost:       " + str(round_(transfer_cost)))
+print("- trade_cost:          " + str(round_(trade_cost)))
+print("- account_update_cost: " + str(round_(account_update_cost)))
+print("- amm_join_exit_cost:  " + str(round_(amm_join_exit_cost)))
