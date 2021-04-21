35c61c52da32b859ba1c6b81eb28d73b7a9cd7f4
kongliangzhong <kongliangzhong@gmail.com>
2021-04-21 16:11:10 +0800

    add genAbi.sh

---
+++ packages/hebao_v2/genAbi.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf ABI/*
+
+solc \
+    -o ABI/ --overwrite \
+    --abi contracts/base/*.sol \
+    --allow-paths contracts/base/libwallet/*.sol contracts/thirdparty/proxy/*.sol contracts/thirdparty/*.sol contracts/lib/*.sol contracts/iface/*.sol
+
+ABI_PATH="ABI"
+
+for file in $ABI_PATH/*
+do
+    rename_file=$(echo $file | awk '{n=split($0,a,"_"); print a[n]}')
+    mv $file $rename_file
+done
