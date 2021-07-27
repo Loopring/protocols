bce0f79f30ac450db22039cb5f31eb4a8635590a
kongliangzhong <kongliangzhong@gmail.com>
2021-07-27 17:00:03 +0800

    Update genAbi.sh

--- packages/hebao_v2/genAbi.sh
+++ packages/hebao_v2/genAbi.sh
@@ -4,6 +4,7 @@ rm -rf ABI/*
 
 solc \
     -o ABI/ --overwrite \
+    --optimize \
     --abi contracts/base/*.sol \
     --allow-paths contracts/base/libwallet/*.sol contracts/thirdparty/proxies/*.sol contracts/thirdparty/*.sol contracts/lib/*.sol contracts/iface/*.sol
 
