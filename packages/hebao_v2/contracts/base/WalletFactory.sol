1c3b397e42408685fa87d68354e33e1327e6ad3e
wangdong <daniel@loopring.org>
2021-07-29 20:15:26 +0800

    Update WalletFactory.sol

--- packages/hebao_v2/contracts/base/WalletFactory.sol
+++ packages/hebao_v2/contracts/base/WalletFactory.sol
@@ -33,7 +33,7 @@ contract WalletFactory is WalletDeployer
         address   feeRecipient;
         address   feeToken;
         uint      maxFeeAmount;
-        uint      feeAmount;
+        uint      salt;
         bytes     signature;
     }
 
@@ -53,14 +53,14 @@ contract WalletFactory is WalletDeployer
     /// @return wallet The new wallet address
     function createWallet(
         WalletConfig calldata config,
-        uint                  salt
+        uint                  feeAmount
         )
         external
         returns (address wallet)
     {
-        _validateRequest(config, salt);
+        _validateRequest(config);
         wallet = _deploy(config.owner, salt);
-        _initializeWallet(wallet, config);
+        _initializeWallet(wallet, config, feeAmount);
     }
 
     /// @dev Computes the wallet address
@@ -86,7 +86,8 @@ contract WalletFactory is WalletDeployer
 
     function _initializeWallet(
         address               wallet,
-        WalletConfig calldata config
+        WalletConfig calldata config,
+        uint                  feeAmount
         )
         internal
     {
@@ -97,7 +98,7 @@ contract WalletFactory is WalletDeployer
             config.inheritor,
             config.feeRecipient,
             config.feeToken,
-            config.feeAmount
+            feeAmount
         );
 
         emit WalletCreated(wallet, config.owner);
@@ -105,7 +106,6 @@ contract WalletFactory is WalletDeployer
 
     function _validateRequest(
         WalletConfig calldata config,
-        uint                  salt
         )
         private
         view
@@ -123,7 +123,7 @@ contract WalletFactory is WalletDeployer
                 config.feeRecipient,
                 config.feeToken,
                 config.maxFeeAmount,
-                salt
+                config.salt
             )
         );
 
