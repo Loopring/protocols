16f6011180b4874e047818c6d9a82776f60e518a
kongliangzhong <kongliangzhong@gmail.com>
2021-09-18 12:10:13 +0800

    Hebaov2 proxy call (#2571)
    * [hebaov2] add some test for proxy call
    
    * revert mainnet contract addresses

---
+++ packages/hebao_v2/contracts/test/PriceOracleDelegate.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright 2017 Loopring Technology Limited.
+pragma solidity ^0.7.0;
+
+import "../iface/PriceOracle.sol";
+
+/// @title PriceOracle
+contract PriceOracleDelegate is PriceOracle
+{
+    PriceOracle public immutable priceOracle;
+
+    constructor(
+        PriceOracle _priceOracle
+        )
+    {
+        priceOracle = _priceOracle;
+    }
+
+    function tokenValue(address token, uint amount)
+        public
+        view
+        override
+        returns (uint value)
+    {
+        return priceOracle.tokenValue(token, amount);
+    }
+}
