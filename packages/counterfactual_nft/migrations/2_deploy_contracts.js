ef27428c000efac7d2fba58b74f10163db4cccdb
Brechtpd <Brechtp.Devos@gmail.com>
2021-11-23 03:07:47 +0100

    Feedback

--- packages/counterfactual_nft/migrations/2_deploy_contracts.js
+++ packages/counterfactual_nft/migrations/2_deploy_contracts.js
@@ -6,15 +6,6 @@ module.exports = async (deployer, network, addresses) => {
 
   const loopringExchangeAddress = "0x0BABA1Ad5bE3a5C0a66E7ac838a129Bf948f1eA4";
 
-  let owner;
-  if (network === 'rinkeby') {
-    owner = addresses[0];
-  } else if (network === 'development') {
-    owner = addresses[0];
-  } else {
-    owner = addresses[0];
-  }
-
   await deployer.deploy(NftContract, loopringExchangeAddress, {gas: 5000000});
   const nft = await NftContract.deployed();
 
