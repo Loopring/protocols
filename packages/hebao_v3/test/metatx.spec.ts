896b928aae7c977067893d1e63addccb259ffc53
john.li <john.li@loopring.io>
2023-04-04 16:58:45 +0800

    rm useless code

--- packages/hebao_v3/test/metatx.spec.ts
+++ packages/hebao_v3/test/metatx.spec.ts
@@ -1,31 +1,25 @@
 import { expect } from "./setup";
 import {
-  MetaTx,
   signCallContractWA,
   signRecover,
-  signMetaTx,
 } from "./helper/signatureUtils";
 import {
   newWallet,
   getFirstEvent,
-  getBlockTimestamp,
   sortSignersAndSignatures,
 } from "./commons";
-// import { /*l2ethers as*/ ethers } from "hardhat";
-const { ethers, artifacts } = require("hardhat");
-import { Contract, Signer, Wallet } from "ethers";
+const { ethers } = require("hardhat");
+import { Contract, Wallet } from "ethers";
 import BN = require("bn.js");
 import fs = require("fs");
-// ethers.
+
 describe("wallet", () => {
   let account1: Wallet;
   let account2: Wallet;
   let account3: Wallet;
   let recoverInterface: any;
   let guardianInterface: any;
-  let metaTxInterface: any;
   let entryPointInterface: any;
-  const feeRecipient = "0x" + "30".repeat(20);
   let TestContract: Contract;
   let LRC: Contract;
 
@@ -57,13 +51,11 @@ describe("wallet", () => {
       const validUntil = new Date().getTime() + 3600 * 24 * 1000;
       const guardian1 = ethers.Wallet.createRandom();
       const guardian2 = ethers.Wallet.createRandom();
-
       let wallet = await newWallet(owner, ethers.constants.AddressZero, 0, [
         guardian1.address,
         guardian2.address,
       ]);
       const masterCopy = await wallet.getMasterCopy();
-
       const newGuardians = ["0x" + "11".repeat(20), "0x" + "22".repeat(20)];
       const sig1 = signRecover(
         masterCopy,
@@ -75,7 +67,6 @@ describe("wallet", () => {
         guardian1.privateKey.slice(2)
       );
       const sig1Bs = Buffer.from(sig1.txSignature.slice(2), "hex");
-
       const sig2 = signRecover(
         masterCopy,
         wallet.address,
@@ -86,25 +77,21 @@ describe("wallet", () => {
         guardian2.privateKey.slice(2)
       );
       const sig2Bs = Buffer.from(sig2.txSignature.slice(2), "hex");
-
       const sortedSigs = sortSignersAndSignatures(
         [guardian1.address, guardian2.address],
         [sig1Bs, sig2Bs]
       );
-
       const approval = {
         signers: sortedSigs.sortedSigners,
         signatures: sortedSigs.sortedSignatures,
         validUntil,
         wallet: wallet.address,
       };
-
       const data = wallet.interface.encodeFunctionData("recover", [
         approval,
         newOwner,
         newGuardians,
       ]);
-
       const tx = await wallet.execute(
         wallet.address,
         ethers.utils.parseEther("0"),
@@ -113,7 +100,6 @@ describe("wallet", () => {
       await tx.wait();
       const ownerAfter = (await wallet.wallet()).owner;
       expect(ownerAfter).to.equal(newOwner);
-
       const guardiansAfter = await wallet.getGuardians(false);
       expect(guardiansAfter[0].addr).to.equal(newGuardians[0]);
       expect(guardiansAfter[1].addr).to.equal(newGuardians[1]);
@@ -122,13 +108,13 @@ describe("wallet", () => {
     it("transfer", async () => {
       const owner = await account1.getAddress();
       let wallet = await newWallet(owner, ethers.constants.AddressZero, 0, []);
+      wallet = wallet.connect(account1);
       await account2.sendTransaction({
         from: await account2.getAddress(),
         to: wallet.address,
         value: ethers.utils.parseEther("100"),
       });
       const transferTo = "0x" + "30".repeat(20);
-      wallet = wallet.connect(account1);
       const toBalanceBefore = await ethers.provider.getBalance(transferTo);
       expect(toBalanceBefore).to.equal(0);
       const tx = await wallet.execute(
@@ -144,6 +130,7 @@ describe("wallet", () => {
     it("transferERC20", async () => {
       const owner = await account1.getAddress();
       let wallet = await newWallet(owner, ethers.constants.AddressZero, 0, []);
+      wallet.connect(account1);
       await account2.sendTransaction({
         from: await account2.getAddress(),
         to: wallet.address,
@@ -153,7 +140,6 @@ describe("wallet", () => {
       const toBalanceBefore = await LRC.functions.balanceOf(transferTo);
       expect(toBalanceBefore.balance).to.equal(0);
       await LRC.setBalance(wallet.address, ethers.utils.parseEther("1000"));
-      wallet = wallet.connect(account1);
       const data = wallet.interface.encodeFunctionData("transferToken", [
         LRC.address,
         transferTo,
@@ -179,26 +165,23 @@ describe("wallet", () => {
         guardian1,
         guardian2,
       ]);
+      wallet = wallet.connect(account1);
       const masterCopy = await wallet.getMasterCopy();
-
       await account2.sendTransaction({
         from: await account2.getAddress(),
         to: wallet.address,
         value: ethers.utils.parseEther("100"),
       });
-
       const validUntil = new Date().getTime() + 1000 * 3600 * 24; // one day
       const callData = TestContract.interface.encodeFunctionData(
         "functionDefault",
         [10]
       );
-
       const getPrivateKey = (address: string) => {
         const textData = fs.readFileSync("./test_account_keys.json", "ascii");
         const data = JSON.parse(textData);
         return data.private_keys[String(address).toLowerCase()];
       }
-
       const sig1 = signCallContractWA(
         masterCopy,
         wallet.address,
@@ -208,10 +191,7 @@ describe("wallet", () => {
         Buffer.from(callData.slice(2), "hex"),
         owner,
         getPrivateKey(owner)
-        // accountOwner.privateKey
-        // account1.
       );
-
       const sig2 = signCallContractWA(
         masterCopy,
         wallet.address,
@@ -221,10 +201,7 @@ describe("wallet", () => {
         Buffer.from(callData.slice(2), "hex"),
         guardian1,
         getPrivateKey(guardian1)
-        // guardians_[0].privateKey
-        // guardian1.privateKey.slice(2)
       );
-
       const sortedSigs = sortSignersAndSignatures(
         [owner, guardian1],
         [
@@ -232,7 +209,6 @@ describe("wallet", () => {
           Buffer.from(sig2.txSignature.slice(2), "hex"),
         ]
       );
-
       const approval = {
         signers: sortedSigs.sortedSigners,
         signatures: sortedSigs.sortedSignatures,
@@ -245,7 +221,6 @@ describe("wallet", () => {
         0,
         callData,
       ]);
-      wallet = wallet.connect(account1);
       const tx = await wallet.execute(
         wallet.address,
         ethers.utils.parseEther("0"),
