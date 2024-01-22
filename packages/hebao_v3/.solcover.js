972155178bbec596c0417e1ed9763a5c25d5162a
breakpoint <kl456123@outlook.com>
2024-01-22 20:22:08 +0800

    fix bug

--- packages/hebao_v3/.solcover.js
+++ packages/hebao_v3/.solcover.js
@@ -1,11 +1,4 @@
 module.exports = {
-  skipFiles: [
-    'account-abstraction/test',
-    'account-abstraction/samples/bls/lib',
-    //solc-coverage fails to compile our Manager module.
-    'account-abstraction/samples/gnosis',
-    'account-abstraction/utils/Exec.sol',
-    'test'
-  ],
+  skipFiles: ['account-abstraction', 'test', 'thirdparty'],
   configureYulOptimizer: true
 }
